7f3704a8a3e51b52a9bafe624b60d17cd9b6f14a
andreasmalta <16372461+andreasmalta@users.noreply.github.com>
2019-03-15 16:54:44 +0100

    initial upload

---
+++ acc5client/tools/chocolateyinstall.ps1
@@ -0,0 +1,24 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'ftp://ftp.avigilon.com/ACC/ACC_5.10.24.2/AvigilonControlCenterClient-5.10.24.2.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+  url64bit      = $url
+
+  softwareName  = 'acc5client*'
+
+  checksum      = 'F9380076060FA2316861EBBC0D0E1C0E25A8068DAE059D9C46598CEFE78F9898'
+  checksumType  = 'sha256'
+  checksum64    = 'F9380076060FA2316861EBBC0D0E1C0E25A8068DAE059D9C46598CEFE78F9898'
+  checksumType64= 'sha256'
+
+  silentArgs    = "/s"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
