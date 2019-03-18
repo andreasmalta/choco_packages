94af19425b3ef107c92f0ae72b50f8fae6f2e5b2
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2019-03-18 10:17:14 +0100

    remove 64bit references

--- acc5client/tools/chocolateyinstall.ps1
+++ acc5client/tools/chocolateyinstall.ps1
@@ -1,4 +1,3 @@
-﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'ftp://ftp.avigilon.com/ACC/ACC_5.10.24.2/AvigilonControlCenterClient-5.10.24.2.exe'
@@ -8,14 +7,11 @@ $packageArgs = @{
   unzipLocation = $toolsDir
   fileType      = 'exe'
   url           = $url
-  url64bit      = $url
 
   softwareName  = 'acc5client*'
 
   checksum      = 'F9380076060FA2316861EBBC0D0E1C0E25A8068DAE059D9C46598CEFE78F9898'
   checksumType  = 'sha256'
-  checksum64    = 'F9380076060FA2316861EBBC0D0E1C0E25A8068DAE059D9C46598CEFE78F9898'
-  checksumType64= 'sha256'
 
   silentArgs    = "/s"
   validExitCodes= @(0, 3010, 1641)
