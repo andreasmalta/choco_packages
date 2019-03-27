d17475b0a48db1e0433bb73f08561fffe6e149bf
andreasmalta <16372461+andreasmalta@users.noreply.github.com>
2019-03-27 16:13:43 +0100

    copy to github

---
+++ acc6client/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'ftp://ftp.avigilon.com/ACC/ACC_6.14.4.6/AvigilonControlCenterClient-6.14.4.6.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'exe'
+  url           = $url
+  softwareName  = 'acc6client*'
+  checksum      = '2567665A4AC01474D506E0B6EDA1A4E5311ABF72BC4552A3724D0AB0A2D94F59'
+  checksumType  = 'sha256'
+  silentArgs    = "/s"
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ acc6client/tools/chocolateyuninstall.ps1
@@ -0,0 +1,1 @@
+﻿Start-Process -FilePath "C:\ProgramData\{B9D2D163-5738-4D49-AC81-EC3B5A3AB01B}\AvigilonControlCenterClient.exe" -ArgumentList "/s MODIFY=FALSE REMOVE=TRUE UNINSTALL=YES" -Wait
