ada5655b7b85297bd38d05e2cf348fdb8122a874
andreasmalta <16372461+andreasmalta@users.noreply.github.com>
2019-03-27 15:16:03 +0100

    uninstall script fixes

--- dwgtrueview/tools/chocolateybeforemodify.ps1
+++ dwgtrueview/tools/chocolateybeforemodify.ps1
@@ -1,1 +1,1 @@
-﻿"MsiExec.exe /qn /norestart /X{28B89EEF-2028-0409-0100-CF3F3A09B77D}"
+﻿Start-Process -FilePath "msiexec.exe" -ArgumentList "/qn /norestart /X{28B89EEF-2028-0409-0100-CF3F3A09B77D}" -Wait
