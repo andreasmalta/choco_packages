888698e8aa4522ddfde9a63f08a3a66ecf155726
andreasmalta <16372461+andreasmalta@users.noreply.github.com>
2019-03-28 11:14:51 +0100

    delete folders without error

--- dwgtrueview.install/tools/chocolateybeforemodify.ps1
+++ dwgtrueview.install/tools/chocolateybeforemodify.ps1
@@ -1,4 +1,2 @@
-﻿$fileLocation = Join-Path $env:ChocolateyInstall 'lib\dwgtrueview.install\tools\DWGTrueView_2019_Enu_32bit\'
-$fileLocation64 = Join-Path $env:ChocolateyInstall 'lib\dwgtrueview.install\tools\DWGTrueView_2019_Enu_64bit\'
-Remove-Item -Recurse -Force $fileLocation
-Remove-Item -Recurse -Force $fileLocation64
+﻿$fileLocation = Join-Path $env:ChocolateyInstall 'lib\dwgtrueview.install\tools\DWGTrueView*'
+Remove-Item $fileLocation -Recurse -Force -ErrorAction Ignore
