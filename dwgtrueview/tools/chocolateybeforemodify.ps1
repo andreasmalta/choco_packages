1e81507322f6669050c0e3bf2df49417008c383c
andreasmalta <16372461+andreasmalta@users.noreply.github.com>
2019-04-04 12:36:24 +0200

    added steps for setup extract

--- dwgtrueview/tools/chocolateybeforemodify.ps1
+++ dwgtrueview/tools/chocolateybeforemodify.ps1
@@ -1,6 +1,12 @@
-﻿$packageArgs = @{
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'DWGTrueView*'
+
+$packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'msi'
   silentArgs    = "{28B89EEF-2028-0409-0100-CF3F3A09B77D} /qn /norestart"
 }
+
 Uninstall-ChocolateyPackage @packageArgs
+Remove-Item $fileLocation -Recurse -Force -ErrorAction Ignore
--- dwgtrueview/tools/chocolateyinstall.ps1
+++ dwgtrueview/tools/chocolateyinstall.ps1
@@ -1,18 +1,38 @@
 ﻿$ErrorActionPreference = 'Stop';
-$fileLocation = Join-Path $env:ChocolateyInstall 'lib\dwgtrueview.install\tools\DWGTrueView_2019_Enu_32bit\Setup.exe'
-$fileLocation64 = Join-Path $env:ChocolateyInstall 'lib\dwgtrueview.install\tools\DWGTrueView_2019_Enu_64bit\Setup.exe'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://download.autodesk.com/esd/dwgtrueview/2019/DWGTrueView_2019_Enu_32bit.sfx.exe'
+$url64      = 'https://download.autodesk.com/esd/dwgtrueview/2019/DWGTrueView_2019_Enu_64bit.sfx.exe'
+$fileLocation = Join-Path $toolsDir 'DWGTrueView_2019_Enu_32bit\Setup.exe'
+$fileLocation64 = Join-Path $toolsDir 'DWGTrueView_2019_Enu_64bit\Setup.exe'
+$checksum      = '60CAC33BCA4FFF14C17FA1E6CAD8CEF0D4D411E949CF0AF1CB46C107C288D590'
+$checksum64    = '66A788BAFFC74B6C17FDD19D9D5D93B8EBC273C78D84F864F485664F31C68A37'
 
-$packageArgs = @{
+
+$packageArgs1 = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'exe'
+  url           = $url
+  url64bit      = $url64
+  softwareName  = 'dwgtrueview*'
+  checksum      = $checksum
+  checksumType  = 'sha256'
+  checksum64    = $checksum64
+  checksumType64= 'sha256'
+  silentArgs    = "-suppresslaunch -d $toolsDir"
+}
+
+$packageArgs2 = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'exe'
   file          = $fileLocation
   file64        = $fileLocation64
   softwareName  = 'dwgtrueview*'
-  checksum      = '60CAC33BCA4FFF14C17FA1E6CAD8CEF0D4D411E949CF0AF1CB46C107C288D590'
+  checksum      = $checksum
   checksumType  = 'sha256'
-  checksum64    = '66A788BAFFC74B6C17FDD19D9D5D93B8EBC273C78D84F864F485664F31C68A37'
+  checksum64    = $checksum64
   checksumType64= 'sha256'
   silentArgs    = "/Q /W"
 }
 
-Install-ChocolateyInstallPackage @packageArgs
+Install-ChocolateyPackage @packageArgs1
+Install-ChocolateyInstallPackage @packageArgs2
