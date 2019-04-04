c448b0bd2c0bfe92aca3a6d951cd2c103bf4f733
andreasmalta <16372461+andreasmalta@users.noreply.github.com>
2019-04-04 14:33:45 +0200

    update install arguments

--- dwgtrueview/tools/chocolateyinstall.ps1
+++ dwgtrueview/tools/chocolateyinstall.ps1
@@ -7,7 +7,6 @@ $fileLocation64 = Join-Path $toolsDir 'DWGTrueView_2019_Enu_64bit\Setup.exe'
 $checksum      = '60CAC33BCA4FFF14C17FA1E6CAD8CEF0D4D411E949CF0AF1CB46C107C288D590'
 $checksum64    = '66A788BAFFC74B6C17FDD19D9D5D93B8EBC273C78D84F864F485664F31C68A37'
 
-
 $packageArgs1 = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'exe'
@@ -31,7 +30,7 @@ $packageArgs2 = @{
   checksumType  = 'sha256'
   checksum64    = $checksum64
   checksumType64= 'sha256'
-  silentArgs    = "/Q /W"
+  silentArgs    = "/S /Q /W"
 }
 
 Install-ChocolateyPackage @packageArgs1
