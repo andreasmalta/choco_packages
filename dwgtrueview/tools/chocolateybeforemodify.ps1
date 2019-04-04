01afa6d52f8f80ff1f7123d3fea539ebb4e31b0c
andreasmalta <16372461+andreasmalta@users.noreply.github.com>
2019-04-04 11:10:18 +0200

    updated uninstall script & license url

--- dwgtrueview/tools/chocolateybeforemodify.ps1
+++ dwgtrueview/tools/chocolateybeforemodify.ps1
@@ -1,1 +1,6 @@
-﻿Start-Process -FilePath "msiexec.exe" -ArgumentList "/qn /norestart /X{28B89EEF-2028-0409-0100-CF3F3A09B77D}" -Wait
+﻿$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'msi'
+  silentArgs    = "{28B89EEF-2028-0409-0100-CF3F3A09B77D} /qn /norestart"
+}
+Uninstall-ChocolateyPackage @packageArgs
