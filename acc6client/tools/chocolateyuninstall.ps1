8c9def510d7775b58d101f1fde95715ea5d52b7d
andreasmalta <16372461+andreasmalta@users.noreply.github.com>
2019-04-04 09:33:42 +0200

    updated uninstall script

--- acc6client/tools/chocolateyuninstall.ps1
+++ acc6client/tools/chocolateyuninstall.ps1
@@ -1,1 +1,7 @@
-﻿Start-Process -FilePath "C:\ProgramData\{B9D2D163-5738-4D49-AC81-EC3B5A3AB01B}\AvigilonControlCenterClient.exe" -ArgumentList "/s MODIFY=FALSE REMOVE=TRUE UNINSTALL=YES" -Wait
+﻿$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'exe'
+  silentArgs    = "/s MODIFY=FALSE REMOVE=TRUE UNINSTALL=YES"
+  file			= 'C:\ProgramData\{B9D2D163-5738-4D49-AC81-EC3B5A3AB01B}\AvigilonControlCenterClient.exe'
+}
+Uninstall-ChocolateyPackage @packageArgs
