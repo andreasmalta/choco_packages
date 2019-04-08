57165a04038c220bbd95c1e4a5228915f7c91973
andreasmalta <16372461+andreasmalta@users.noreply.github.com>
2019-04-08 17:07:57 +0200

    updated version and uninstall script

--- dwgtrueview/tools/chocolateybeforemodify.ps1
+++ dwgtrueview/tools/chocolateybeforemodify.ps1
@@ -5,7 +5,7 @@ $fileLocation = Join-Path $toolsDir 'DWGTrueView*'
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'msi'
-  silentArgs    = "{28B89EEF-2028-0409-0100-CF3F3A09B77D} /qn /norestart"
+  silentArgs    = "{28B89EEF-3028-0409-0100-CF3F3A09B77D} /qn /norestart"
 }
 
 Uninstall-ChocolateyPackage @packageArgs
