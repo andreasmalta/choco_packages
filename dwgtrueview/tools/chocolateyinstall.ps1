44461bf90459f49f850f0c342a6d1800b3af1313
andreasmalta <16372461+andreasmalta@users.noreply.github.com>
2019-04-08 14:12:19 +0200

    update to 2020

--- dwgtrueview/tools/chocolateyinstall.ps1
+++ dwgtrueview/tools/chocolateyinstall.ps1
@@ -1,21 +1,16 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://download.autodesk.com/esd/dwgtrueview/2019/DWGTrueView_2019_Enu_32bit.sfx.exe'
-$url64      = 'https://download.autodesk.com/esd/dwgtrueview/2019/DWGTrueView_2019_Enu_64bit.sfx.exe'
-$fileLocation = Join-Path $toolsDir 'DWGTrueView_2019_Enu_32bit\Setup.exe'
-$fileLocation64 = Join-Path $toolsDir 'DWGTrueView_2019_Enu_64bit\Setup.exe'
-$checksum      = '60CAC33BCA4FFF14C17FA1E6CAD8CEF0D4D411E949CF0AF1CB46C107C288D590'
-$checksum64    = '66A788BAFFC74B6C17FDD19D9D5D93B8EBC273C78D84F864F485664F31C68A37'
+$url      = 'https://trial2.autodesk.com/NetSWDLD/2020/ACD/79D9B4F2-2A92-453A-A1A4-CF3F3A09B77D/SFX/DWGTrueView_2020_Enu_64bit_dlm.sfx.exe'
+$checksum_url    = 'B8576EB643C879CB078F6779D45131625285664D9621B00C0B02B00073383FAD'
+$file = Join-Path $toolsDir 'DWGTrueView_2020_Enu_64bit_dlm\Setup.exe'
+$checksum_file    = 'A42B7914F3834871C84404961432BADF11A8E839BB19BA2014EF8F91202DA5B0'
 
 $packageArgs1 = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'exe'
-  url           = $url
-  url64bit      = $url64
+  url64bit      = $url
   softwareName  = 'dwgtrueview*'
-  checksum      = $checksum
-  checksumType  = 'sha256'
-  checksum64    = $checksum64
+  checksum64    = $checksum_url
   checksumType64= 'sha256'
   silentArgs    = "-suppresslaunch -d $toolsDir"
 }
@@ -23,12 +18,9 @@ $packageArgs1 = @{
 $packageArgs2 = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'exe'
-  file          = $fileLocation
-  file64        = $fileLocation64
+  file64        = $file
   softwareName  = 'dwgtrueview*'
-  checksum      = $checksum
-  checksumType  = 'sha256'
-  checksum64    = $checksum64
+  checksum64    = $checksum_file
   checksumType64= 'sha256'
   silentArgs    = "/S /Q /W"
 }
