cc6f74ae622475a4a0dd16df3d6b05947a7c3a99
andreasmalta <16372461+andreasmalta@users.noreply.github.com>
2019-05-09 13:42:47 +0200

    testing

---
+++ solarwinds-tftp-server/tools/chocolateyinstall.ps1
@@ -0,0 +1,28 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url      = 'https://downloads.solarwinds.com/solarwinds/Release/FreeTool/SolarWinds-TFTP-Server.zip'
+$checksum_url    = 'BBDC2D1FDCCEBB445F972480675DF732A674FC86671EBE7107161D8DAA3DFA14'
+$extract = Join-Path $toolsDir 'setup'
+$file = Join-Path $toolsDir 'setup\TFTP-Server-Installer.exe'
+$checksum_file    = 'EC61640CA19CD915B264058198237802477D3B44B4D62B35A654087AA31B8C03'
+
+$packageArgs1 = @{
+  packageName   = $env:ChocolateyPackageName
+  url           = $url
+  checksum      = $checksum_url
+  checksumType  = 'sha256'
+  unziplocation = $extract
+}
+Install-ChocolateyZipPackage @packageArgs1
+
+$packageArgs2 = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'exe'
+  file          = $file
+  softwareName  = 'SolarWinds TFTP Server*'
+  checksum      = $checksum_file
+  checksumType  = 'sha256'
+  silentArgs    = "/S /R"
+  validExitCodes= @(0, 3010, 1641)
+}
+Install-ChocolateyInstallPackage @packageArgs2
