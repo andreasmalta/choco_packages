3b75626f7894de8a01a5ccc5df0af5156cadca4f
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2019-10-21 22:41:13 +0200

    initial release

--- geit5pcl6/tools/chocolateyinstall.ps1
+++ geit5pcl6/tools/chocolateyinstall.ps1
@@ -2,10 +2,10 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'https://dl.develop.eu/en/?tx_kmanacondaimport_downloadproxy[fileId]=fda2b9f7de235182ba423627216161f0&tx_kmanacondaimport_downloadproxy[documentId]=1483&tx_kmanacondaimport_downloadproxy[system]=Develop&tx_kmanacondaimport_downloadproxy[language]=EN&type=1558521685'
 $url64      = 'https://dl.develop.eu/en/?tx_kmanacondaimport_downloadproxy[fileId]=31153230a299b1c3939e5518e04e20a9&tx_kmanacondaimport_downloadproxy[documentId]=1482&tx_kmanacondaimport_downloadproxy[system]=Develop&tx_kmanacondaimport_downloadproxy[language]=EN&type=1558521685'
+
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  fileType      = 'EXE_MSI_OR_MSU' #only one of these: exe, msi, msu
   url           = $url
   url64bit      = $url64
   checksum      = 'C95A5AE3A115346BEE4944E8DA1CBBF7449BF8DE32AA6A95E9803012BEFBDC64'
@@ -14,3 +14,11 @@ $packageArgs = @{
   checksumType64= 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs
+
+Get-ChildItem $toolsDir -Recurse -Filter "*.inf" | 
+ForEach-Object { PNPUtil.exe /add-driver $_.FullName /install }
+Add-PrinterDriver -Name "Generic 28C-8SeriesPCL"
+Add-PrinterDriver -Name "Generic 36C-9SeriesPCL"
+Add-PrinterDriver -Name "Generic 65C-9SeriesPCL"
+Add-PrinterDriver -Name "Generic 70C-10SeriesPCL"
+Add-PrinterDriver -Name "Generic C MF385-2SeriesPCL"
