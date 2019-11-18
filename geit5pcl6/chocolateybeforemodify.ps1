da77e328b77b1ea5fb86323dbf07f945e62ff065
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2019-11-18 08:40:11 +0100

    Delete chocolateybeforemodify.ps1

--- geit5pcl6/chocolateybeforemodify.ps1
+++
@@ -1,5 +0,0 @@
-﻿$ErrorActionPreference = 'Stop';
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileLocation = Join-Path $toolsDir 'GEIT5*'
-
-Remove-Item $fileLocation -Recurse -Force -ErrorAction Ignore
