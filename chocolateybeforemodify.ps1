8d5f07e8069733ffbed873ceed8f01b5ff0537bb
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2019-11-18 08:40:07 +0100

    Create chocolateybeforemodify.ps1

---
+++ chocolateybeforemodify.ps1
@@ -0,0 +1,5 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'GEIT5*'
+
+Remove-Item $fileLocation -Recurse -Force -ErrorAction Ignore
