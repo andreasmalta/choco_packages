8bb24e62695925b3bab10ca636d5f464f9cd8b98
andreasmalta <16372461+andreasmalta@users.noreply.github.com>
2020-03-26 16:19:23 +0100

    uninstall fix

---
+++ flashbackexpress/tools/chocolateyuninstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'exe'
+  silentArgs    = "/S"
+  file			= 'C:\Program Files (x86)\Blueberry Software\FlashBack Express 5\uninstall.exe'
+}
+Uninstall-ChocolateyPackage @packageArgs
