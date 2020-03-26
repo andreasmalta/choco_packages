4ee0721d2428a9a7b33ed8ed56ea8179eaff3e7a
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2020-03-26 23:16:33 +0100

    test unzip

--- dwgtrueview/tools/chocolateyinstall.ps1
+++ dwgtrueview/tools/chocolateyinstall.ps1
@@ -16,33 +16,34 @@ Install-ChocolateyZipPackage @packageArgs1
 
 #$file = Join-Path $toolsDir 'DWGTrueView_2020_Enu_64bit_dlm\Setup.exe'
 #$checksum_file    = 'A42B7914F3834871C84404961432BADF11A8E839BB19BA2014EF8F91202DA5B0'
-$RegRebootRequired = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired"
+#$RegRebootRequired = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired"
 
-$packageArgs1 = @{
-  packageName   = $env:ChocolateyPackageName
-  fileType      = 'exe'
-  url           = $url
-  softwareName  = 'dwgtrueview*'
-  checksum      = '757C80DCD13D71F1EC09065BDB8F1E0F6994B773231440C008107DCAF4E3C78E'
-  checksumType  = 'sha256'
-  silentArgs    = "-suppresslaunch -d $toolsDir"
-  validExitCodes= @(0, 3010, 1641)
-}
+#$packageArgs1 = @{
+#  packageName   = $env:ChocolateyPackageName
+#  fileType      = 'exe'
+#  url           = $url
+#  softwareName  = 'dwgtrueview*'
+#  checksum      = '757C80DCD13D71F1EC09065BDB8F1E0F6994B773231440C008107DCAF4E3C78E'
+#  checksumType  = 'sha256'
+#  silentArgs    = "-suppresslaunch -d $toolsDir"
+#  validExitCodes= @(0, 3010, 1641)
+#}
 
-$packageArgs2 = @{
-  packageName   = $env:ChocolateyPackageName
-  fileType      = 'exe'
-  file          = $file
-  softwareName  = 'dwgtrueview*'
-  checksum      = '757C80DCD13D71F1EC09065BDB8F1E0F6994B773231440C008107DCAF4E3C78E'
-  checksumType  = 'sha256'
-  silentArgs    = "/q /w"
-  validExitCodes= @(0, 3010, 1641)
-}
+#$packageArgs2 = @{
+#  packageName   = $env:ChocolateyPackageName
+#  fileType      = 'exe'
+#  file          = $file
+#  softwareName  = 'dwgtrueview*'
+#  checksum      = '757C80DCD13D71F1EC09065BDB8F1E0F6994B773231440C008107DCAF4E3C78E'
+#  checksumType  = 'sha256'
+#  silentArgs    = "/q /w"
+#  validExitCodes= @(0, 3010, 1641)
+#}
 
-if (Test-path $RegRebootRequired)
-{
-    Remove-Item -Path $RegRebootRequired
-}
-Install-ChocolateyPackage @packageArgs1
-Install-ChocolateyInstallPackage @packageArgs2
+#if (Test-path $RegRebootRequired)
+#{
+#    Remove-Item -Path $RegRebootRequired
+#}
+#Install-ChocolateyPackage @packageArgs1
+#Install-ChocolateyInstallPackage @packageArgs2
+#
