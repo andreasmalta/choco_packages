224d37b06c4c2db4272f3d73e8a2ee9e41a38ba4
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2020-03-26 22:32:27 +0100

    Update chocolateyinstall.ps1

--- kinovea/tools/chocolateyinstall.ps1
+++ kinovea/tools/chocolateyinstall.ps1
@@ -1,18 +1,18 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://www.kinovea.org/setup/kinovea.0.9.1/Kinovea-0.9.1-x64.exe'
+$url      = 'https://www.kinovea.org/setup/kinovea.0.9.1/Kinovea-0.9.1-x64.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe'
-  url64bit      = $url64
+  url           = $url
 
   softwareName  = 'kinovea*'
 
-  checksum64    = 'B8BB787178E4804CCDDEF4144A70D76A94F34F5E06904C5DE906EC5C55EC5C53'
-  checksumType64= 'sha256'
+  checksum      = 'B8BB787178E4804CCDDEF4144A70D76A94F34F5E06904C5DE906EC5C55EC5C53'
+  checksumType  = 'sha256'
 
   silentArgs    = "/S"
   validExitCodes= @(0, 3010, 1641)
