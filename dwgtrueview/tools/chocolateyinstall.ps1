8964e56dcacba6739167fd1e262ccf3e7f4f7eb8
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2020-03-26 23:11:32 +0100

    version2021

--- dwgtrueview/tools/chocolateyinstall.ps1
+++ dwgtrueview/tools/chocolateyinstall.ps1
@@ -1,18 +1,30 @@
 ﻿$ErrorActionPreference = 'Stop';
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url      = 'https://trial2.autodesk.com/NetSWDLD/2020/ACD/79D9B4F2-2A92-453A-A1A4-CF3F3A09B77D/SFX/DWGTrueView_2020_Enu_64bit_dlm.sfx.exe'
-$checksum_url    = 'B8576EB643C879CB078F6779D45131625285664D9621B00C0B02B00073383FAD'
-$file = Join-Path $toolsDir 'DWGTrueView_2020_Enu_64bit_dlm\Setup.exe'
-$checksum_file    = 'A42B7914F3834871C84404961432BADF11A8E839BB19BA2014EF8F91202DA5B0'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url            = 'https://trial2.autodesk.com/NetSWDLD/2021/ACD/E6A1E1F6-4270-4D64-8BD5-CF3F3A09B77D/SFX/DWGTrueView_2021_English_64bit_dlm.sfx.exe'
+
+$packageArgs1 = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  url           = $url
+  checksum      = '757C80DCD13D71F1EC09065BDB8F1E0F6994B773231440C008107DCAF4E3C78E'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs1
+
+
+
+#$file = Join-Path $toolsDir 'DWGTrueView_2020_Enu_64bit_dlm\Setup.exe'
+#$checksum_file    = 'A42B7914F3834871C84404961432BADF11A8E839BB19BA2014EF8F91202DA5B0'
 $RegRebootRequired = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired"
 
 $packageArgs1 = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'exe'
-  url64bit      = $url
+  url           = $url
   softwareName  = 'dwgtrueview*'
-  checksum64    = $checksum_url
-  checksumType64= 'sha256'
+  checksum      = '757C80DCD13D71F1EC09065BDB8F1E0F6994B773231440C008107DCAF4E3C78E'
+  checksumType  = 'sha256'
   silentArgs    = "-suppresslaunch -d $toolsDir"
   validExitCodes= @(0, 3010, 1641)
 }
@@ -20,10 +32,10 @@ $packageArgs1 = @{
 $packageArgs2 = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'exe'
-  file64        = $file
+  file          = $file
   softwareName  = 'dwgtrueview*'
-  checksum64    = $checksum_file
-  checksumType64= 'sha256'
+  checksum      = '757C80DCD13D71F1EC09065BDB8F1E0F6994B773231440C008107DCAF4E3C78E'
+  checksumType  = 'sha256'
   silentArgs    = "/q /w"
   validExitCodes= @(0, 3010, 1641)
 }
