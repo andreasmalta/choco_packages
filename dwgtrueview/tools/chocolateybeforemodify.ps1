7daf69938cbe6dfe6f634fbbffa26b60ff0668cb
andreasmalta <16372461+andreasmalta@users.noreply.github.com>
2020-03-27 15:25:00 +0100

    v2021 ready

--- dwgtrueview/tools/chocolateybeforemodify.ps1
+++ dwgtrueview/tools/chocolateybeforemodify.ps1
@@ -2,11 +2,6 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileLocation = Join-Path $toolsDir 'DWGTrueView*'
 
-$packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  fileType      = 'msi'
-#  silentArgs    = "{28B89EEF-3028-0409-0100-CF3F3A09B77D} /qn /norestart"
-}
-
-Uninstall-ChocolateyPackage @packageArgs
+Start-Process -FilePath "msiexec.exe" -ArgumentList "/x {28B89EEF-3028-0409-0100-CF3F3A09B77D} /qn /norestart" -Wait
+Start-Process -FilePath "msiexec.exe" -ArgumentList "/x {28B89EEF-4128-0409-0100-CF3F3A09B77D} /qn /norestart" -Wait
 Remove-Item $fileLocation -Recurse -Force -ErrorAction Ignore
--- dwgtrueview/tools/chocolateyinstall.ps1
+++ dwgtrueview/tools/chocolateyinstall.ps1
@@ -1,49 +1,34 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url            = 'https://trial2.autodesk.com/NetSWDLD/2021/ACD/E6A1E1F6-4270-4D64-8BD5-CF3F3A09B77D/SFX/DWGTrueView_2021_English_64bit_dlm.sfx.exe'
+$file           = Join-Path $toolsDir 'DWGTrueView_2021_English_64bit_dlm\Setup.exe'
+$RegRebootRequired = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired"
 
 $packageArgs1 = @{
   packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
+  fileType      = 'exe'
   url           = $url
-  checksum      = '757C80DCD13D71F1EC09065BDB8F1E0F6994B773231440C008107DCAF4E3C78E'
+  softwareName  = 'dwgtrueview*'
+  checksum      = '47DBDE0EA38F195433902836D3944906519B34384D058452CC7F8A893A793480'
   checksumType  = 'sha256'
+  silentArgs    = "-suppresslaunch -d $toolsDir"
+  validExitCodes= @(0, 3010, 1641)
 }
 
-Install-ChocolateyZipPackage @packageArgs1
+$packageArgs2 = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'exe'
+  file          = $file
+  softwareName  = 'dwgtrueview*'
+  checksum      = '32AA0B3585DCA9FD5ED45CA527B16EDB358393406E1AD9D48CC766BD18EED44A'
+  checksumType  = 'sha256'
+  silentArgs    = "/q /w"
+  validExitCodes= @(0, 3010, 1641)
+}
 
-
-
-#$file = Join-Path $toolsDir 'DWGTrueView_2020_Enu_64bit_dlm\Setup.exe'
-#$checksum_file    = 'A42B7914F3834871C84404961432BADF11A8E839BB19BA2014EF8F91202DA5B0'
-#$RegRebootRequired = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired"
-
-#$packageArgs1 = @{
-#  packageName   = $env:ChocolateyPackageName
-#  fileType      = 'exe'
-#  url           = $url
-#  softwareName  = 'dwgtrueview*'
-#  checksum      = '757C80DCD13D71F1EC09065BDB8F1E0F6994B773231440C008107DCAF4E3C78E'
-#  checksumType  = 'sha256'
-#  silentArgs    = "-suppresslaunch -d $toolsDir"
-#  validExitCodes= @(0, 3010, 1641)
-#}
-
-#$packageArgs2 = @{
-#  packageName   = $env:ChocolateyPackageName
-#  fileType      = 'exe'
-#  file          = $file
-#  softwareName  = 'dwgtrueview*'
-#  checksum      = '757C80DCD13D71F1EC09065BDB8F1E0F6994B773231440C008107DCAF4E3C78E'
-#  checksumType  = 'sha256'
-#  silentArgs    = "/q /w"
-#  validExitCodes= @(0, 3010, 1641)
-#}
-
-#if (Test-path $RegRebootRequired)
-#{
-#    Remove-Item -Path $RegRebootRequired
-#}
-#Install-ChocolateyPackage @packageArgs1
-#Install-ChocolateyInstallPackage @packageArgs2
-#
+if (Test-path $RegRebootRequired)
+{
+    Remove-Item -Path $RegRebootRequired
+}
+Install-ChocolateyPackage @packageArgs1
+Install-ChocolateyInstallPackage @packageArgs2
