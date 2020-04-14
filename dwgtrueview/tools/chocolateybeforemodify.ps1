c715e71a89439e5f7103ee1b1e5eb6f785474240
andreasmalta <16372461+andreasmalta@users.noreply.github.com>
2020-04-14 10:55:05 +0200

    remove msiexec

--- dwgtrueview/tools/chocolateybeforemodify.ps1
+++ dwgtrueview/tools/chocolateybeforemodify.ps1
@@ -2,6 +2,18 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileLocation = Join-Path $toolsDir 'DWGTrueView*'
 
-Start-Process -FilePath "msiexec.exe" -ArgumentList "/x {28B89EEF-3028-0409-0100-CF3F3A09B77D} /qn /norestart" -Wait
-Start-Process -FilePath "msiexec.exe" -ArgumentList "/x {28B89EEF-4128-0409-0100-CF3F3A09B77D} /qn /norestart" -Wait
+$packageArgs2020 = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'msi'
+  silentArgs    = "{28B89EEF-3028-0409-0100-CF3F3A09B77D} /qn /norestart"
+}
+
+$packageArgs2021 = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'msi'
+  silentArgs    = "{28B89EEF-4128-0409-0100-CF3F3A09B77D} /qn /norestart"
+}
+
+Uninstall-ChocolateyPackage @packageArgs2020
+Uninstall-ChocolateyPackage @packageArgs2021
 Remove-Item $fileLocation -Recurse -Force -ErrorAction Ignore
