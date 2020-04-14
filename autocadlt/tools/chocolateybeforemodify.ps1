b0298683f50f68586907b94c0294b46e50c67a35
andreasmalta <16372461+andreasmalta@users.noreply.github.com>
2020-04-14 16:04:33 +0200

    create autocadlt

---
+++ autocadlt/tools/chocolateybeforemodify.ps1
@@ -0,0 +1,53 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'DWGTrueView*'
+
+$AutoCADLT2021English = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'msi'
+  silentArgs    = "{28B89EEF-4109-0409-2102-CF3F3A09B77D} /qn /norestart"
+}
+
+$AutodeskSingleSignOnComponent = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'msi'
+  silentArgs    = "{951BB060-1350-4C93-BD83-D966C51D4005} /qn /norestart"
+}
+
+$AutodeskSavetoWebandMobile = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'msi'
+  silentArgs    = "{A9005AC0-4AD8-4E84-B1F7-EE38BB6BCC2D} /qn /norestart"
+}
+
+$AutodeskMaterialLibrary2021 = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'msi'
+  silentArgs    = "{6774FD60-7D4B-4D57-BE56-2702A07C9701} /qn /norestart"
+}
+
+$AutodeskMaterialLibraryBaseResolutionImageLibrary2021 = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'msi'
+  silentArgs    = "{6EFAD582-86C1-4AB2-97C5-2070D0B90E08} /qn /norestart"
+}
+
+$AutodeskGenuineService = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'msi'
+  silentArgs    = "{54A00624-3EF9-49A2-92A9-7244EADD0212} /qn /norestart"
+}
+
+Uninstall-ChocolateyPackage @AutoCADLT2021English
+Uninstall-ChocolateyPackage @AutodeskSingleSignOnComponent
+Uninstall-ChocolateyPackage @AutodeskSavetoWebandMobile
+Uninstall-ChocolateyPackage @AutodeskMaterialLibrary2021
+Uninstall-ChocolateyPackage @AutodeskMaterialLibraryBaseResolutionImageLibrary2021
+Stop-Process -Name "Autodesk*" -Force
+Stop-Service -Name "AdAppMgrSvc"
+Remove-Item –path C:\ProgramData\Autodesk\SDS –recurse
+& "C:\Program Files (x86)\Autodesk\Autodesk Desktop App\removeAdAppMgr.exe" --mode unattended
+Set-Date -Date (Get-Date).AddDays(1)
+Uninstall-ChocolateyPackage @AutodeskGenuineService
+Set-Date -Date (Get-Date).AddDays(-1)
+Remove-Item $fileLocation -Recurse -Force -ErrorAction Ignore
---
+++ autocadlt/tools/chocolateyinstall.ps1
@@ -0,0 +1,34 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url            = 'https://efulfillment.autodesk.com/NetSWDLD/2021/ACDLT/B1A1D73D-D894-441D-803D-BFFCF11A1340/SFX/AutoCAD_LT_2021_SWL_English_Win_64bit_dlm.sfx.exe'
+$file           = Join-Path $toolsDir 'AutoCAD_LT_2021_SWL_English_Win_64bit_dlm\Setup.exe'
+$RegRebootRequired = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired"
+
+$packageArgsExtract = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'exe'
+  url           = $url
+  softwareName  = 'autocadlt*'
+  checksum      = 'B7B6551918F352D7D8010DE77F3DD869A2B5E774F7D628066BB446634DF9E5E3'
+  checksumType  = 'sha256'
+  silentArgs    = "-suppresslaunch -d $toolsDir"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+$packageArgsSetup = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'exe'
+  file          = $file
+  softwareName  = 'autocadlt*'
+  checksum      = '32AA0B3585DCA9FD5ED45CA527B16EDB358393406E1AD9D48CC766BD18EED44A'
+  checksumType  = 'sha256'
+  silentArgs    = "/q /w"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+if (Test-path $RegRebootRequired)
+{
+    Remove-Item -Path $RegRebootRequired
+}
+Install-ChocolateyPackage @packageArgsExtract
+Install-ChocolateyInstallPackage @packageArgsSetup
