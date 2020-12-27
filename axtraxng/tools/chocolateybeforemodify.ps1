11196525d7b5efbf0993b4eb047807585affd1e7
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2020-12-27 17:39:47 +0100

    configtool uninstall

--- axtraxng/tools/chocolateybeforemodify.ps1
+++ axtraxng/tools/chocolateybeforemodify.ps1
@@ -4,3 +4,9 @@ $extract_url = Join-Path $toolsDir 'zip'
 $extract_exe = Join-Path $toolsDir 'exe'
 Remove-Item $extract_url -Recurse -Force -ErrorAction Ignore
 Remove-Item $extract_exe -Recurse -Force -ErrorAction Ignore
+
+$packageArgsConfigTool = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = 'AxTraxNG Configuration Tool'
+}
+Uninstall-ChocolateyPackage @packageArgsConfigTool
