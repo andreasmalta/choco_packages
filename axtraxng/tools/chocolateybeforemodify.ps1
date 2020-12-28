aef155719aeff2e621f463c766428a1f8b640133
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2020-12-28 18:54:41 +0100

    new source

--- axtraxng/tools/chocolateybeforemodify.ps1
+++ axtraxng/tools/chocolateybeforemodify.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$extract_url = Join-Path $toolsDir 'exe'
+$extract_url = Join-Path $toolsDir 'axtraxng-master'
 Remove-Item $extract_url -Recurse -Force -ErrorAction Ignore
 
 [array]$key = Get-UninstallRegistryKey -SoftwareName "AxTraxNG Configuration Tool"
--- axtraxng/tools/chocolateyinstall.ps1
+++ axtraxng/tools/chocolateyinstall.ps1
@@ -2,17 +2,16 @@
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $url = 'https://github.com/andreasmalta/axtraxng/archive/master.zip'
-$checksum_url = '704D634C008EA295A5F30C2D6B2A4ABA2FC8C2E741F053E4CCE822DFF5F37C64'
-$extract_url = Join-Path $toolsDir 'exe'
+$checksum_url = 'DB5516E075A9BF581B14417871FB156321E77854DC6BD024413519AB5F2A1ED3'
 $packageArgsURL = @{
   packageName   = $env:ChocolateyPackageName
   url           = $url
   checksum      = $checksum_url
   checksumType  = 'sha256'
-  unziplocation = $extract_url
+  unziplocation = $toolsDir
 }
 
-$file_Client = Join-Path $toolsDir 'exe\Client\setup.exe'
+$file_Client = Join-Path $toolsDir 'axtraxng-master\Client\setup.exe'
 $checksum_Client = 'F6603E4BDFCB5AF04EA9679CABF08B390CC932CF7B970658082D599E54BD0CE9'
 $packageArgsClient = @{
   packageName   = $env:ChocolateyPackageName
@@ -25,7 +24,7 @@ $packageArgsClient = @{
   validExitCodes= @(0, 3010, 1641)
 }
 
-$file_ConfigTool = Join-Path $toolsDir 'exe\AxTraxConfigTool\setup.exe'
+$file_ConfigTool = Join-Path $toolsDir 'axtraxng-master\AxTraxConfigTool\setup.exe'
 $checksum_ConfigTool = '81AFBDAA2C39F1A80FEB806C05517DB56F71C1388AD5ABE006D1426E8050480D'
 $packageArgsConfigTool = @{
   packageName   = $env:ChocolateyPackageName
@@ -38,7 +37,7 @@ $packageArgsConfigTool = @{
   validExitCodes= @(0, 3010, 1641)
 }
 
-$file_Monitor = Join-Path $toolsDir 'exe\Server Monitor\setup.exe'
+$file_Monitor = Join-Path $toolsDir 'axtraxng-master\Server Monitor\setup.exe'
 $checksum_Monitor = '47492CE43E5633788505D425534388D3181EC8FCBF4FA8B71E0C05AAA72E02DD'
 $packageArgsMonitor = @{
   packageName   = $env:ChocolateyPackageName
@@ -51,7 +50,7 @@ $packageArgsMonitor = @{
   validExitCodes= @(0, 3010, 1641)
 }
 
-$file_PreReq = Join-Path $toolsDir 'exe\Pre_Requisite\setup.exe'
+$file_PreReq = Join-Path $toolsDir 'axtraxng-master\Pre_Requisite\setup.exe'
 $checksum_PreReq = '8991F94D60CDF190026B542CA9324DCA530373BB22CD8F4550A7A5C6B2E423A4'
 $packageArgsPreReq = @{
   packageName   = $env:ChocolateyPackageName
@@ -64,7 +63,7 @@ $packageArgsPreReq = @{
   validExitCodes= @(0, 3010, 1641)
 }
 
-$file_Server = Join-Path $toolsDir 'exe\Server\setup.exe'
+$file_Server = Join-Path $toolsDir 'axtraxng-master\Server\setup.exe'
 $checksum_Server = 'A60937DCE5598F4C34D8474CF31035FB6FA97DEC156AD337D3E0EC311C538DAB'
 $packageArgsServer = @{
   packageName   = $env:ChocolateyPackageName
