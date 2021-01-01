542f0ec91d607c3359c1c8428cba89fe525db8da
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2021-01-01 22:39:03 +0100

    eula

--- navfree/Readme/html/scripts/adsk.indexes-data.js
+++
@@ -1,8 +0,0 @@
-$(document).trigger("register-index-data", [[
-{ l: 'C', c: [
-{ l: 'copyright', f: './files/GUID-D926B320-D928-454C-BC80-ED710452AB0D.htm'}
-]},
-{ l: 'T', c: [
-{ l: 'trademarks', f: './files/GUID-D926B320-D928-454C-BC80-ED710452AB0D.htm'}
-]}
-]]);
--- navfree/Readme/html/scripts/toc-treedata.js
+++
@@ -1,9 +0,0 @@
-$(document).trigger("register-toc-data", ["", [
-{data:"Autodesk Navisworks 2021 Readme", attr:{id:"d1e26", tn:0, pn:undefined, prn:undefined, nxn:1, href:"./files/GUID-C7592226-3363-4CA3-9E42-7EC825AFF91E.htm", desc:""}, children:[
-{data:"Installing Autodesk Navisworks", attr:{id:"d1e40", tn:1, pn:0, prn:0, nxn:2, href:"./files/GUID-3BFAEE60-5EA9-4A73-82D9-B9C8E2CD7EF6.htm", desc:"", rel:"page"}},
-{data:"Customer Involvement Program", attr:{id:"d1e51", tn:2, pn:0, prn:1, nxn:4, href:"./files/GUID-827D6E9D-92DB-45A5-BA6C-26177DAAE45E.htm", desc:""}, children:[
-{data:"Customer Involvement Program", attr:{id:"d1e102", tn:3, pn:2, prn:undefined, nxn:undefined, href:"./files/GUID-79DAC916-CA90-4148-B1AD-1413CA0BACBF.htm", desc:"", rel:"page"}}]},
-{data:"Product Notes", attr:{id:"d1e135", tn:4, pn:0, prn:2, nxn:5, href:"./files/GUID-9FD7B989-2F99-4E46-A2BC-EC551706E5DA.htm", desc:"", rel:"page"}},
-{data:"Known Problems in Autodesk Navisworks 2021", attr:{id:"d1e150", tn:5, pn:0, prn:4, nxn:6, href:"./files/GUID-A5356A8D-B6CD-4A61-84DF-1F16387BC1D0.htm", desc:"", rel:"page"}},
-{data:"Credits", attr:{id:"d1e167", tn:6, pn:0, prn:5, nxn:7, href:"./files/GUID-185F01F6-70B8-4F62-8645-C36519F42BBA.htm", desc:"", rel:"page"}},
-{data:"Autodesk Legal Notice", attr:{id:"d1e189", tn:7, pn:0, prn:6, nxn:undefined, href:"./files/GUID-D926B320-D928-454C-BC80-ED710452AB0D.htm", desc:"", rel:"page"}}]}]]);
