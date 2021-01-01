2265df9c0842c3f2d52bd5c387d80309a53618e8
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2021-01-01 23:48:38 +0100

    v2021

---
+++ navfree/tools/chocolateybeforemodify.ps1
@@ -0,0 +1,10 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'extract'
+Remove-Item $fileLocation -Recurse -Force -ErrorAction Ignore
+
+$navfree = Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -like "Autodesk Navisworks Freedom*" }
+$navfree.Uninstall()
+
+$matlib = Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -like "Autodesk Material Library*" }
+$matlib.Uninstall()
