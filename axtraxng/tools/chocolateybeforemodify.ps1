e2874e6a7ab2f0f2717f2788f8b32021103488a1
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2021-01-02 23:25:29 +0100

    Get-UninstallRegistryKey

--- axtraxng/tools/chocolateybeforemodify.ps1
+++ axtraxng/tools/chocolateybeforemodify.ps1
@@ -3,5 +3,19 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $extract_url = Join-Path $toolsDir 'axtraxng-master'
 Remove-Item $extract_url -Recurse -Force -ErrorAction Ignore
 
-[array]$key = Get-UninstallRegistryKey -SoftwareName "AxTraxNG Configuration Tool"
-$key.UninstallString
+$packageName = $env:ChocolateyPackageName
+$softwareName = 'AxTraxNG Configuration Tool'
+$installerType = 'msi'
+$silentArgs = '/qn /norestart'
+$validExitCodes = @(0, 3010, 1605, 1614, 1641)
+[array]$key = Get-UninstallRegistryKey -SoftwareName $softwareName
+$file = "$($key.UninstallString)"
+$silentArgs = "$($key.PSChildName) $silentArgs"
+$file = ''
+
+Uninstall-ChocolateyPackage `
+	-PackageName $packageName `
+	-FileType $installerType `
+	-SilentArgs "$silentArgs" `
+	-ValidExitCodes $validExitCodes `
+	-File "$file"
