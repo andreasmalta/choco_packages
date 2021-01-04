b4cdc7893e8a0ec6fac6c5f06362fc6030fd9c69
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2021-01-04 12:30:20 +0100

    v2020.09.29

---
+++ moldflowcommunicator/tools/chocolateybeforemodify.ps1
@@ -0,0 +1,4 @@
+﻿$ErrorActionPreference = 'Stop';
+
+#Uninstall old version - Autodesk Moldflow Communicator 2021
+Uninstall-ChocolateyPackage -PackageName "Autodesk Moldflow Communicator 2021" -FileType "msi" -SilentArgs "{753C779D-0564-4042-8000-8FF1C120CB5E} /qn /norestart"
---
+++ moldflowcommunicator/tools/chocolateyinstall.ps1
@@ -0,0 +1,31 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$url            = 'https://damassets.autodesk.net/content/dam/autodesk/www/products/moldflow/files/MFC_2021_FCS_Multilingual_Win_64bit_dlm.sfx.exe'
+$checksum_url   = '3339302E1925EDCED8965879BB7236C3E7635A4899E8E8345336F68DEBDA468F'
+$file           = Join-Path $env:TEMP 'MFC_2021_FCS_Multilingual_Win_64bit_dlm\setup.exe'
+$checksum_file  = '32AA0B3585DCA9FD5ED45CA527B16EDB358393406E1AD9D48CC766BD18EED44A'
+
+$packageArgsURL = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'exe'
+  url           = $url
+  softwareName  = 'autodesk moldflow communicator*'
+  checksum      = $checksum_url
+  checksumType  = 'sha256'
+  silentArgs    = "-suppresslaunch -d $env:TEMP"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+$packageArgsFile = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'exe'
+  file          = $file
+  softwareName  = 'autodesk moldflow communicator*'
+  checksum      = $checksum_file
+  checksumType  = 'sha256'
+  silentArgs    = "/q /w"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgsURL
+Install-ChocolateyInstallPackage @packageArgsFile
