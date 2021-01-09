4ebceb46f88f4713b631aa1908ffb308f0835156
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2021-01-09 22:22:13 +0100

    v1.6.16

--- brightauthorconnected/tools/chocolateyinstall.ps1
+++ brightauthorconnected/tools/chocolateyinstall.ps1
@@ -1,14 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop';
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url      = 'https://brightsignnetwork.com/download/gostosh/ba-connected/BAconnected-Setup-1.6.6.exe'
+$url            = 'https://brightsignnetwork.com/download/gostosh/ba-connected/BAconnected-Setup-1.6.16.exe'
+$checksum_url   = '4D8CD437C402071482F35DBE8753E5FD87E7292AD8A7B1EC0CF32A0626B834D4'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
+  unzipLocation = $env:TEMP
   fileType      = 'exe'
   url           = $url
   softwareName  = 'BAconnected*'
-  checksum      = 'D60BC066A2D6C32322573A298D366D34E54A1523DEFE4049F9D5CCA701F87004'
+  checksum      = $checksum_url
   checksumType  = 'sha256'
   silentArgs    = "/S"
   validExitCodes= @(0, 3010, 1641)
