ff920c291abbf326ccd89cb27c32ae20f5088fa2
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2021-02-09 23:29:05 +0100

    Delete chocolateybeforemodify.ps1

--- moldflowc/tools/chocolateybeforemodify.ps1
+++
@@ -1,4 +0,0 @@
-﻿$ErrorActionPreference = 'Stop';
-
-#Uninstall old version - Autodesk Moldflow Communicator 2021
-Uninstall-ChocolateyPackage -PackageName "Autodesk Moldflow Communicator 2021" -FileType "msi" -SilentArgs "{753C779D-0564-4042-8000-8FF1C120CB5E} /qn /norestart"
