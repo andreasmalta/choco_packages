cbc0c1ebf5c558e1db538b1545649ad0d41834b6
Andersson Andreas <Andersson.Andreas@voestalpine.com>
2021-02-20 23:22:24 +0100

    hpsupportassistant

---
+++ hpsupportassistant/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop';
+
+#$url            = 'https://ftp.hp.com/pub/softpaq/sp111501-112000/sp111562.exe'
+$url            = 'C:\temp\sp111562.exe'
+$checksum_url   = '0052CA213A8B1A8A6BC77102160DC529FF24BD14337510CCDDC73C9EA7469692'
+$packageArgsURL = @{
+  packageName   = $env:ChocolateyPackageName
+  file          = $url
+  checksum      = $checksum_url
+  checksumType  = 'sha256'
+  unziplocation = $env:TEMP
+}
+Install-ChocolateyZipPackage @packageArgsURL
+
+#run setup from directory
+cd $env:TEMP; Start-Process -Wait -FilePath ".\Setup.exe" -ArgumentList "-s"
