f28362717517ce54aa662975e527374453247975
Andersson Andreas <Andersson.Andreas@voestalpine.com>
2021-02-21 13:58:07 +0100

    add uninstall

---
+++ hpsupportassistant/tools/chocolateybeforemodify.ps1
@@ -0,0 +1,4 @@
+﻿$ErrorActionPreference = 'Stop';
+
+#support assistant upgrade & uninstallation is not silent
+Start-Process -Wait -FilePath "C:\Program Files (x86)\Hewlett-Packard\HP Support Framework\UninstallHPSA.exe" -ArgumentList "/s /v /qn"
--- hpsupportassistant/tools/chocolateyinstall.ps1
+++ hpsupportassistant/tools/chocolateyinstall.ps1
@@ -1,7 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-#$url            = 'https://ftp.hp.com/pub/softpaq/sp111501-112000/sp111562.exe'
-$url            = 'C:\temp\sp111562.exe'
+$url            = 'https://ftp.hp.com/pub/softpaq/sp111501-112000/sp111562.exe'
 $checksum_url   = '0052CA213A8B1A8A6BC77102160DC529FF24BD14337510CCDDC73C9EA7469692'
 $packageArgsURL = @{
   packageName   = $env:ChocolateyPackageName
@@ -12,5 +11,6 @@ $packageArgsURL = @{
 }
 Install-ChocolateyZipPackage @packageArgsURL
 
-#run setup from directory
+#apparently needs to open in a new window otherwise it closes down the choco process (start-process -wait)
+#apparently needs to run from the same directory to work (cd $env:temp)
 cd $env:TEMP; Start-Process -Wait -FilePath ".\Setup.exe" -ArgumentList "-s"
