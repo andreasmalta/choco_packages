8d227bb9c48f6092d406133e21e803f0f5832ff1
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2021-02-24 14:15:55 +0100

    desktopconnector

---
+++ desktopconnector/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$url            = 'https://up.autodesk.com/2017/CDX/AB4AADCC-F890-4B4F-A7A6-B0FBD2386796/DesktopConnector-x64.exe'
+$checksum_url   = '27B0BBD26B061645A9360B7AF514D51A9060D42A79484CEF8D75980A3CD0194D'
+$packageArgsURL = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'exe'
+  url           = $url
+  softwareName  = 'autodesk desktop connector*'
+  checksum      = $checksum_url
+  checksumType  = 'sha256'
+  silentArgs    = "-s"
+  validExitCodes= @(0, 3010, 1641)
+}
+Install-ChocolateyPackage @packageArgsURL
---
+++ desktopconnector/tools/chocolateyuninstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop';
+
+#not removed automatically:
+$packageName = 'Autodesk Single Sign On Component*'
+$validExitCodes = @(0, 3010, 1605, 1614, 1641)
+Get-ItemProperty -Path @('HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                         'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*') `
+                 -ErrorAction:SilentlyContinue `
+| Where-Object   {$_.DisplayName -like $packageName} `
+| ForEach-Object {
+	$silentArgs = '/qn /norestart'
+	$file = "$($_.UninstallString)"
+	$silentArgs = "$($_.PSChildName) $silentArgs"
+	$file = ''
+	Uninstall-ChocolateyPackage -PackageName "$packageName" -FileType "msi" -SilentArgs "$($silentArgs)" -File "$file" -ValidExitCodes $validExitCodes
+	}
