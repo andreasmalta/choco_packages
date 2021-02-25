180d325bb592a3319beb38fd71dd20abf7da2cf3
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2021-02-25 21:30:50 +0100

    v5.02.0027

--- deskupdate/tools/chocolateyinstall.ps1
+++ deskupdate/tools/chocolateyinstall.ps1
@@ -1,17 +1,16 @@
 ﻿$ErrorActionPreference = 'Stop';
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://webdownloads3.ts.fujitsu.com/download/FileDownload/fileDownload.aspx?SoftwareGUID=ADB78416-CEC6-4914-83AB-81244A2A2939&FileFolder=Downloadfiles&FileTypeExtension=EXE&FileNameClient=FTS_DeskUpdateSetup_5020026_1244560.EXE'
+$url             = 'https://webdownloads2.ts.fujitsu.com/download/FileDownload/fileDownload.aspx?SoftwareGUID=5345FCD8-3F02-4199-90AD-22269D958EF8&FileFolder=Downloadfiles&FileTypeExtension=EXE&FileNameClient=FTS_DeskUpdateSetup_5020027_1249345.EXE'
+$checksum_url    = '285641F909F12552068BDEBF3BF8F78F9916331356101B23A563DD9B16C577BC'
 
-$packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  fileType      = 'exe'
-  url           = $url
-  softwareName  = 'deskupdate*'
-  checksum      = 'F9E97B9B289E56F5E54B44D934A59E0D5BA78A4618BD91149C231C5CF1483ABE'
-  checksumType  = 'sha256'
-  silentArgs    = "/VERYSILENT"
-  validExitCodes= @(0, 3010, 1641)
+$packageArgs     = @{
+  packageName    = $env:ChocolateyPackageName
+  softwareName   = 'deskupdate*'
+  unzipLocation  = $env:TEMP
+  fileType       = 'exe'
+  url            = $url
+  checksum       = $checksum_url
+  checksumType   = 'sha256'
+  silentArgs     = "/VERYSILENT"
+  validExitCodes = @(0, 3010, 1641)
 }
-
 Install-ChocolateyPackage @packageArgs
