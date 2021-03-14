4638732af5d0bf4f0a2e25579e413d4d2d56c93d
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2021-03-14 11:38:48 +0100

    hpdiagnostics

--- hpdiagnostics/tools/chocolateyinstall.ps1
+++ hpdiagnostics/tools/chocolateyinstall.ps1
@@ -1,16 +1,36 @@
 ﻿$ErrorActionPreference = 'Stop';
-$url            = 'https://ftp.hp.com/pub/softpaq/sp112501-113000/sp112517.exe'
-$checksum_url   = '22C97A9269395072950037B58C76AD63ACD702D7AEA3152DA2B24F6890EE554C'
+$installdir     = Join-Path -Path $env:LocalAppData -ChildPath "Temp\1\chocolatey" | Join-Path -ChildPath $env:ChocolateyPackageName | Join-Path -ChildPath $env:ChocolateyPackageVersion
+$copyinstall  = Join-Path -Path $env:LocalAppData -ChildPath "Temp\chocolatey" | Join-Path -ChildPath $env:ChocolateyPackageName | Join-Path -ChildPath $env:ChocolateyPackageVersion
+
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$download       = 'https://ftp.hp.com/pub/softpaq/sp112501-113000/sp112517.exe'
+$checksum       = '22C97A9269395072950037B58C76AD63ACD702D7AEA3152DA2B24F6890EE554C'
 
 $packageArgsURL = @{
   packageName   = $env:ChocolateyPackageName
-  file          = $url
-  checksum      = $checksum_url
+  url           = $download
+  checksum      = $checksum
   checksumType  = 'sha256'
-  unziplocation = $env:TEMP
+  unziplocation = $toolsDir
 }
 Install-ChocolateyZipPackage @packageArgsURL
 
-#apparently needs to open in a new window otherwise it closes down the choco process (start-process -wait)
-#apparently needs to run from the same directory to work (cd $env:temp)
-cd $env:TEMP; Start-Process -Wait -FilePath ".\Setup.exe" -ArgumentList "-s"
+$extract1       = Join-Path $toolsDir 'setup.exe'
+$packageArgsExtract = @{
+  packageName   = $env:ChocolateyPackageName
+  file          = $extract1
+  unziplocation = $installdir
+}
+Install-ChocolateyZipPackage @packageArgsExtract
+
+Get-ChildItem $installdir | Copy-Item -Destination $copyinstall -Recurse -Force
+$extract2       = Join-Path $installdir 'setup.exe'
+$packageArgsInstall = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'exe'
+  file          = $extract2
+  softwareName  = 'hp pc hardware diagnostics*'
+  silentArgs    = "/qn"
+  validExitCodes= @(0, 3010, 1641)
+}
+Install-ChocolateyPackage @packageArgsInstall
