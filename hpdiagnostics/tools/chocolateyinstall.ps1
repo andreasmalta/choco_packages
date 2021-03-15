a1be3ed72ea780ee2cc0342267590971dde25346
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2021-03-15 11:14:47 +0100

    hpdiagnostics

--- hpdiagnostics/tools/chocolateyinstall.ps1
+++ hpdiagnostics/tools/chocolateyinstall.ps1
@@ -1,34 +1,31 @@
 ﻿$ErrorActionPreference = 'Stop';
-$installdir     = Join-Path -Path $env:LocalAppData -ChildPath "Temp\1\chocolatey" | Join-Path -ChildPath $env:ChocolateyPackageName | Join-Path -ChildPath $env:ChocolateyPackageVersion
-$copyinstall  = Join-Path -Path $env:LocalAppData -ChildPath "Temp\chocolatey" | Join-Path -ChildPath $env:ChocolateyPackageName | Join-Path -ChildPath $env:ChocolateyPackageVersion
-
-$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $download       = 'https://ftp.hp.com/pub/softpaq/sp112501-113000/sp112517.exe'
 $checksum       = '22C97A9269395072950037B58C76AD63ACD702D7AEA3152DA2B24F6890EE554C'
 
+$extract        = Join-Path $env:TEMP 'extract'
 $packageArgsURL = @{
   packageName   = $env:ChocolateyPackageName
   url           = $download
   checksum      = $checksum
   checksumType  = 'sha256'
-  unziplocation = $toolsDir
+  unziplocation = $extract
 }
 Install-ChocolateyZipPackage @packageArgsURL
 
-$extract1       = Join-Path $toolsDir 'setup.exe'
+$setup1         = Join-Path $extract 'setup.exe'
+$installdir     = Join-Path -Path $env:LocalAppData -ChildPath "Temp\1\chocolatey" | Join-Path -ChildPath $env:ChocolateyPackageName | Join-Path -ChildPath $env:ChocolateyPackageVersion
 $packageArgsExtract = @{
   packageName   = $env:ChocolateyPackageName
-  file          = $extract1
+  file          = $setup1
   unziplocation = $installdir
 }
 Install-ChocolateyZipPackage @packageArgsExtract
 
-Get-ChildItem $installdir | Copy-Item -Destination $copyinstall -Recurse -Force
-$extract2       = Join-Path $installdir 'setup.exe'
+$setup2         = Join-Path $installdir 'setup.exe'
 $packageArgsInstall = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'exe'
-  file          = $extract2
+  file          = $setup2
   softwareName  = 'hp pc hardware diagnostics*'
   silentArgs    = "/qn"
   validExitCodes= @(0, 3010, 1641)
