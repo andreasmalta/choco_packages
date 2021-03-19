eef9ecef587954a4303053e060e802db95b3cb3b
Andersson Andreas <Andersson.Andreas@voestalpine.com>
2021-03-19 07:03:19 +0100

    v1.5.0.0

--- fbxreview/tools/chocolateyinstall.ps1
+++ fbxreview/tools/chocolateyinstall.ps1
@@ -1,19 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-#$url            = 'https://dl2.filehorse.com/win/imaging-and-digital-photo/autodesk-fbx-review/Autodesk-FBX-Review-1.5.0.0.exe?st=5quuOVzZIw0KBhHn6xqG6A&e=1616066636&fn=fbx-review-installer.exe'
-$url            = 'C:\packages\fbxreview\fbx-review-installer.exe'
+$url            = 'https://dl2.filehorse.com/win/imaging-and-digital-photo/autodesk-fbx-review/Autodesk-FBX-Review-1.5.0.0.exe?st=ORxIVm6xf5yYd3oNg07kBQ&e=1616219897&fn=fbx-review-installer.exe'
 $checksum_url   = 'C87887125A61FF3BCA636BBEC495E3BAE64AFAE27E4492E4DC2907ECB9F7BD14'
 
 #remove version 1.4.1 before upgrading
-$packageArgs141 = @{
-packageName   = $env:ChocolateyPackageName
-fileType      = 'msi'
-silentArgs    = "{689A5161-B50F-45F3-8434-E87790E26D9C} /qn /norestart"
-}
-If(-not(Test-Path -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{689A5161-B50F-45F3-8434-E87790E26D9C}")){
-ECHO "HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH"
-Uninstall-ChocolateyPackage $packageArgs141
+$installedversion = [version](Get-Command "C:\Program Files\Autodesk\FBX Review\fbxreview.exe").FileVersionInfo.FileVersion
+if($installedversion -eq "1.4.1.0") {
+Uninstall-ChocolateyPackage -PackageName $env:ChocolateyPackageName -FileType 'msi' -SilentArgs "{689A5161-B50F-45F3-8434-E87790E26D9C} /qn /norestart" -ValidExitCodes @(0, 3010, 1605, 1614, 1641) -File ""
 }
 
 #1.5.0 install not silent, using autohotkey script
@@ -30,5 +24,5 @@ $packageArgs = @{
   silentArgs    = "/S"
   validExitCodes= @(0, 3010, 1641)
 }
-#Start-Process $ahkEXE $ahkFile
-#Install-ChocolateyPackage @packageArgs
+Start-Process $ahkEXE $ahkFile
+Install-ChocolateyPackage @packageArgs
