16ba77139ebd0ff1d71641a802b73dd1c963e101
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2021-03-20 20:01:23 +0100

    Test-Path

--- fbxreview/tools/chocolateyinstall.ps1
+++ fbxreview/tools/chocolateyinstall.ps1
@@ -5,9 +5,11 @@ $url            = 'https://dl2.filehorse.com/win/imaging-and-digital-photo/autod
 $checksum_url   = 'C87887125A61FF3BCA636BBEC495E3BAE64AFAE27E4492E4DC2907ECB9F7BD14'
 
 #remove version 1.4.1 before upgrading
-$installedversion = [version](Get-Command "C:\Program Files\Autodesk\FBX Review\fbxreview.exe").FileVersionInfo.FileVersion
-if($installedversion -eq "1.4.1.0") {
-Uninstall-ChocolateyPackage -PackageName $env:ChocolateyPackageName -FileType 'msi' -SilentArgs "{689A5161-B50F-45F3-8434-E87790E26D9C} /qn /norestart" -ValidExitCodes @(0, 3010, 1605, 1614, 1641) -File ""
+$fbxreviewpath = "C:\Program Files\Autodesk\FBX Review\fbxreview.exe"
+if (Test-Path $fbxreviewpath -PathType leaf) {
+$fbxreviewversion = [version](Get-Command $fbxreviewpath).FileVersionInfo.FileVersion
+if($fbxreviewversion -eq "1.4.1.0") {
+Uninstall-ChocolateyPackage -PackageName $env:ChocolateyPackageName -FileType 'msi' -SilentArgs "{689A5161-B50F-45F3-8434-E87790E26D9C} /qn /norestart" -ValidExitCodes @(0, 3010, 1605, 1614, 1641) -File ""}
 }
 
 #1.5.0 install not silent, using autohotkey script
