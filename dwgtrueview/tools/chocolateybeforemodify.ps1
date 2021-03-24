99e2b1aa97c6bd195146af198f61e98e8ddf9ef5
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2021-03-24 06:05:00 +0100

    v2022

--- dwgtrueview/tools/chocolateybeforemodify.ps1
+++ dwgtrueview/tools/chocolateybeforemodify.ps1
@@ -1,19 +1,16 @@
 ﻿$ErrorActionPreference = 'Stop';
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileLocation = Join-Path $toolsDir 'DWGTrueView*'
 
-$packageArgs2020 = @{
-  packageName   = $env:ChocolateyPackageName
-  fileType      = 'msi'
-  silentArgs    = "{28B89EEF-3028-0409-0100-CF3F3A09B77D} /qn /norestart"
-}
-
-$packageArgs2021 = @{
-  packageName   = $env:ChocolateyPackageName
-  fileType      = 'msi'
-  silentArgs    = "{28B89EEF-4128-0409-0100-CF3F3A09B77D} /qn /norestart"
-}
-
-Uninstall-ChocolateyPackage @packageArgs2020
-Uninstall-ChocolateyPackage @packageArgs2021
-Remove-Item $fileLocation -Recurse -Force -ErrorAction Ignore
+#old versions are not removed automatically
+$packageName = 'DWGTrueView*'
+$validExitCodes = @(0, 3010, 1605, 1614, 1641)
+Get-ItemProperty -Path @('HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                         'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*') `
+                 -ErrorAction:SilentlyContinue `
+| Where-Object   {$_.DisplayName -like $packageName} `
+| ForEach-Object {
+	$silentArgs = '/qn /norestart'
+	$file = "$($_.UninstallString)"
+	$silentArgs = "$($_.PSChildName) $silentArgs"
+	$file = ''
+	Uninstall-ChocolateyPackage -PackageName "$packageName" -FileType "msi" -SilentArgs "$($silentArgs)" -File "$file" -ValidExitCodes $validExitCodes
+	}
--- dwgtrueview/tools/chocolateyinstall.ps1
+++ dwgtrueview/tools/chocolateyinstall.ps1
@@ -1,31 +1,48 @@
 ﻿$ErrorActionPreference = 'Stop';
-$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'https://trial2.autodesk.com/NetSWDLD/2021/ACD/E6A1E1F6-4270-4D64-8BD5-CF3F3A09B77D/SFX/DWGTrueView_2021_English_64bit_dlm.sfx.exe'
-$file           = Join-Path $toolsDir 'DWGTrueView_2021_English_64bit_dlm\Setup.exe'
-$RegRebootRequired = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired"
+
+$pp = Get-PackageParameters
+if ($pp.'French') {
+Write-Host "INSTALLING FRENCH DWG TRUEVIEW"
+$url           = 'https://efulfillment.autodesk.com/NetSWDLD/2022/ACD/D7A6621A-1A6A-3DAC-BBD2-9EB566035195/SFX/DWGTrueView_2022_English_64bit_dlm.sfx.exe'
+$checksum      = '847731F2FD7C8E6DE98618E3C51F9D0E5E47CBC990B713BA1FB620AC1E09D3C3'
+$unzip         = Join-Path $env:TEMP 'DWGTrueView_2021_English_64bit_dlm\Setup.exe'
+}
+
+elseif ($pp.'Japanese') {
+Write-Host "INSTALLING JAPANESE DWG TRUEVIEW"
+$url           = 'https://efulfillment.autodesk.com/NetSWDLD/2022/ACD/D7A6621A-1A6A-3DAC-BBD2-9EB566035195/SFX/DWGTrueView_2022_English_64bit_dlm.sfx.exe'
+$checksum      = '847731F2FD7C8E6DE98618E3C51F9D0E5E47CBC990B713BA1FB620AC1E09D3C3'
+$unzip         = Join-Path $env:TEMP 'DWGTrueView_2021_English_64bit_dlm\Setup.exe'
+}
+
+else {
+Write-Host "INSTALLING ENGLISH DWG TRUEVIEW"
+$url           = 'https://efulfillment.autodesk.com/NetSWDLD/2022/ACD/D7A6621A-1A6A-3DAC-BBD2-9EB566035195/SFX/DWGTrueView_2022_English_64bit_dlm.sfx.exe'
+$checksum      = '847731F2FD7C8E6DE98618E3C51F9D0E5E47CBC990B713BA1FB620AC1E09D3C3'
+$unzip         = Join-Path $env:TEMP 'DWGTrueView_2021_English_64bit_dlm\Setup.exe'
+}
 
 $packageArgs1 = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'exe'
   url           = $url
   softwareName  = 'dwgtrueview*'
-  checksum      = '47DBDE0EA38F195433902836D3944906519B34384D058452CC7F8A893A793480'
+  checksum      = $checksum
   checksumType  = 'sha256'
-  silentArgs    = "-suppresslaunch -d $toolsDir"
+  silentArgs    = "-suppresslaunch -d $env:TEMP"
   validExitCodes= @(0, 3010, 1641)
 }
 
 $packageArgs2 = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'exe'
-  file          = $file
+  file          = $unzip
   softwareName  = 'dwgtrueview*'
-  checksum      = '32AA0B3585DCA9FD5ED45CA527B16EDB358393406E1AD9D48CC766BD18EED44A'
-  checksumType  = 'sha256'
   silentArgs    = "/q /w"
   validExitCodes= @(0, 3010, 1641)
 }
 
+$RegRebootRequired = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired"
 if (Test-path $RegRebootRequired)
 {
     Remove-Item -Path $RegRebootRequired
