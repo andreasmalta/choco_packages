96209c65fdb8af62d18a7eb2000ea9cef9443d5a
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2021-03-25 15:42:54 +0100

    v2022

---
+++ dwgtrueview/tools/chocolateybeforemodify.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop';
+
+#Uninstall DWG TrueView 2022 before upgrade
+$packageName = 'DWG TrueView 2022*'
+$validExitCodes = @(0, 3010, 1605, 1614, 1641)
+Get-ItemProperty -Path @('HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                         'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*') `
+                 -ErrorAction:SilentlyContinue `
+| Where-Object   {$_.DisplayName -like $packageName} `
+| ForEach-Object {
+	$silentArgs = '/qn /norestart'
+	$file = "$($_.UninstallString)"
+	$silentArgs = "$($_.PSChildName) $silentArgs"
+	$file = ''
+	Uninstall-ChocolateyPackage -PackageName "$packageName" -FileType "msi" -SilentArgs "$($silentArgs)" -File "$file" -ValidExitCodes $validExitCodes
+	}
