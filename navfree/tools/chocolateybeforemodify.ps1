840c80e6abe4e053b86032149c4b5fc3ea64a6e8
Andersson Andreas <Andersson.Andreas@voestalpine.com>
2021-03-26 22:58:41 +0100

    v2022

--- navfree/tools/chocolateybeforemodify.ps1
+++ navfree/tools/chocolateybeforemodify.ps1
@@ -1,19 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-#Uninstall old version - Main software & English Language Pack 2022
-$packageName = 'Autodesk Navisworks Freedom*'
-$validExitCodes = @(0, 3010, 1605, 1614, 1641)
-Get-ItemProperty -Path @('HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
-                         'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*') `
-                 -ErrorAction:SilentlyContinue `
-| Where-Object   {$_.DisplayName -like $packageName} `
-| ForEach-Object {
-	$silentArgs = '/qn /norestart'
-	$file = "$($_.UninstallString)"
-	$silentArgs = "$($_.PSChildName) $silentArgs"
-	$file = ''
-	Uninstall-ChocolateyPackage -PackageName "$packageName" -FileType "msi" -SilentArgs "$($silentArgs)" -File "$file" -ValidExitCodes $validExitCodes
-	}
+#Uninstall old version - Autodesk Navisworks Freedom & Language Packs
+Uninstall-ChocolateyPackage -PackageName "Autodesk Navisworks Freedom" -FileType "msi" -SilentArgs "{C39E6EC4-985A-0000-92D4-F6DC06B39DD2} /qn /norestart"
+Remove-Item 'C:\Program Files\Autodesk\Navisworks Freedom 2022' -Recurse -Force -ErrorAction Ignore
+Remove-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{C39E6EC4-985A-0409-92D4-F6DC06B39DD2}" -Recurse
 
 #Uninstall old version - Material Library
 $packageName = '*Material Library*'
--- navfree/tools/chocolateyinstall.ps1
+++ navfree/tools/chocolateyinstall.ps1
@@ -2,93 +2,90 @@
 $url             = 'https://efulfillment.autodesk.com/NetSWDLD/2022/NAVFREE/087B95DF-F444-3F18-B633-4A7A0A0488A4/SFX/Autodesk_Navisworks_Freedom_2022_Win_64bit_dlm.sfx.exe'
 $checksum        = 'F490744780CD2F986AED31643F85818736E3680F71D47681E563C13C5894103A'
 
-$unzip           = Join-Path $env:TEMP 'DWGTrueView_2022_English_64bit_dlm'
-$navfree         = Join-Path $unzip 'x64\NAVFREE\freedom.msi'
-$navfreelp       = Join-Path $unzip 'x64\en-US\NAVFREE\NAVFREE_LP.msi'
+$unzip           = Join-Path $env:TEMP 'Autodesk_Navisworks_Freedom_2022_Win_64bit_dlm'
 $matlib          = Join-Path $unzip 'Content\ADSKMaterials\CM\MaterialLibrary2022.msi'
 $matlibbase      = Join-Path $unzip 'Content\ADSKMaterials\ILB\BaseImageLibrary2022.msi'
 $matlibadv       = Join-Path $unzip 'Content\ADSKMaterials\PLB\PrismBaseImageLibrary2022.msi'
 $adsso           = Join-Path $unzip 'x64\AdSSO\AdSSO.msi'
+$navfree         = Join-Path $unzip 'x64\NAVFREE\freedom.msi'
+$navfreelp       = Join-Path $unzip 'x64\en-US\NAVFREE\NAVFREE_LP.msi'
 $ags             = Join-Path $unzip 'x64\AGS\Autodesk Genuine Service.msi'
 $adapp           = Join-Path $unzip 'x86\ADSKAPP\AdApplicationManager-installer.exe'
 
 $navfreekey      = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{C39E6EC4-985A-0000-92D4-F6DC06B39DD2}'
-$languagekey     = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{C39E6EC4-985A-0409-92D4-F6DC06B39DD2}'
 $uninstallstring = 'MsiExec.exe /I{C39E6EC4-985A-0000-92D4-F6DC06B39DD2}'
 
 $packageArgsUnzip = @{
-  packageName    = $env:ChocolateyPackageName
+  packageName    = 'NAVFREE Unzip'
   fileType       = 'exe'
   url            = $url
-  softwareName   = 'NavFree Unzip'
+  softwareName   = 'NAVFREE Unzip*'
   checksum       = $checksum
   checksumType   = 'sha256'
   silentArgs     = "-suppresslaunch -d $env:TEMP"
   validExitCodes = @(0, 3010, 1641)
 }
-
-$packageArgsNavFree  = @{
-  packageName    = $env:ChocolateyPackageName
-  fileType       = 'msi'
-  file           = $navfree
-  softwareName   = 'dwgtrueview*'
-  silentArgs     = "/qn /norestart"
-  validExitCodes = @(0, 3010, 1641)
-}
-
-$packageArgsNavFreeLP  = @{
-  packageName    = $env:ChocolateyPackageName
-  fileType       = 'msi'
-  file           = $navfreelp
-  softwareName   = 'dwgtrueview*'
-  silentArgs     = "/qn /norestart"
-  validExitCodes = @(0, 3010, 1641)
-}
 $packageArgsMatLib  = @{
-  packageName    = $env:ChocolateyPackageName
+  packageName    = 'Autodesk Material Library'
   fileType       = 'msi'
   file           = $matlib
-  softwareName   = 'dwgtrueview*'
+  softwareName   = 'Autodesk Material Library*'
   silentArgs     = "/qn /norestart"
   validExitCodes = @(0, 3010, 1641)
 }
 $packageArgsMatLibBase  = @{
-  packageName    = $env:ChocolateyPackageName
+  packageName    = 'Autodesk Material Library Base Resolution Image Library'
   fileType       = 'msi'
   file           = $matlibbase
-  softwareName   = 'dwgtrueview*'
+  softwareName   = 'Autodesk Material Library Base Resolution Image Library*'
   silentArgs     = "/qn /norestart"
   validExitCodes = @(0, 3010, 1641)
 }
 $packageArgsMatLibAdv  = @{
-  packageName    = $env:ChocolateyPackageName
+  packageName    = 'Autodesk Advanced Material Library Base Resolution Image Library'
   fileType       = 'msi'
   file           = $matlibadv
-  softwareName   = 'dwgtrueview*'
+  softwareName   = 'Autodesk Advanced Material Library Base Resolution Image Library*'
   silentArgs     = "/qn /norestart"
   validExitCodes = @(0, 3010, 1641)
 }
 $packageArgsAdSSO  = @{
-  packageName    = $env:ChocolateyPackageName
+  packageName    = 'Autodesk Single Sign On Component'
   fileType       = 'msi'
   file           = $adsso
-  softwareName   = 'dwgtrueview*'
+  softwareName   = 'Autodesk Single Sign On Component*'
+  silentArgs     = "/qn /norestart"
+  validExitCodes = @(0, 3010, 1641)
+}
+$packageArgsNavFree  = @{
+  packageName    = 'Autodesk Navisworks Freedom'
+  fileType       = 'msi'
+  file           = $navfree
+  softwareName   = 'Autodesk Navisworks Freedom*'
+  silentArgs     = "/qn /norestart"
+  validExitCodes = @(0, 3010, 1641)
+}
+$packageArgsNavFreeLP  = @{
+  packageName    = 'Autodesk Navisworks Freedom Language Pack'
+  fileType       = 'msi'
+  file           = $navfreelp
+  softwareName   = 'Autodesk Navisworks Freedom Language Pack*'
   silentArgs     = "/qn /norestart"
   validExitCodes = @(0, 3010, 1641)
 }
 $packageArgsAGS  = @{
-  packageName    = $env:ChocolateyPackageName
+  packageName    = 'Autodesk Genuine Service'
   fileType       = 'msi'
   file           = $ags
-  softwareName   = 'dwgtrueview*'
+  softwareName   = 'Autodesk Genuine Service*'
   silentArgs     = "/qn /norestart"
   validExitCodes = @(0, 3010, 1641)
 }
 $packageArgsAdApp = @{
-  packageName    = $env:ChocolateyPackageName
+  packageName    = 'Autodesk Desktop App'
   fileType       = 'exe'
   file           = $adapp
-  softwareName   = 'NavFree Unzip'
+  softwareName   = 'Autodesk Desktop App*'
   silentArgs     = "--mode unattended"
   validExitCodes = @(0, 3010, 1641)
 }
@@ -102,12 +99,12 @@ if (Test-path $RegRebootRequired)
 
 #unzip and install (using msi since setup.exe not silent)
 Install-ChocolateyPackage @packageArgsUnzip
-Install-ChocolateyInstallPackage @packageArgsNavFree
-Install-ChocolateyInstallPackage @packageArgsNavFreeLP
 Install-ChocolateyInstallPackage @packageArgsMatLib
 Install-ChocolateyInstallPackage @packageArgsMatLibBase
 Install-ChocolateyInstallPackage @packageArgsMatLibAdv
 Install-ChocolateyInstallPackage @packageArgsAdSSO
+Install-ChocolateyInstallPackage @packageArgsNavFree
+Install-ChocolateyInstallPackage @packageArgsNavFreeLP
 Install-ChocolateyInstallPackage @packageArgsAGS
 Install-ChocolateyInstallPackage @packageArgsAdApp
 
@@ -117,8 +114,4 @@ if (Test-path $navfreekey)
 Set-Itemproperty -path $navfreekey -Name 'NoRemove' -value '0' -Type dword
 Set-Itemproperty -path $navfreekey -Name 'SystemComponent' -value '0' -Type dword
 Set-ItemProperty -path $navfreekey -name 'UninstallString' -value $uninstallstring -Type ExpandString
-}
-if (Test-path $languagekey)
-{
-Set-Itemproperty -path $languagekey -Name 'SystemComponent' -value '0' -Type dword
 }
