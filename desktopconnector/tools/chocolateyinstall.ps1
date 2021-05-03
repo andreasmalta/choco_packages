6890df16ee96481ceb3663e4b4ed1cb698d65c7d
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2021-05-03 09:23:38 +0200

    v14.9

--- desktopconnector/tools/chocolateyinstall.ps1
+++ desktopconnector/tools/chocolateyinstall.ps1
@@ -1,19 +1,42 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $url            = 'https://up.autodesk.com/2017/CDX/AB4AADCC-F890-4B4F-A7A6-B0FBD2386796/DesktopConnector-x64.exe'
-$checksum_url   = '1C0272A2F88D8F1F5AF0C7984ED2A6E071DB44EFD99C548133DE7F70396A91EB'
-$packageArgsURL = @{
-  packageName   = $env:ChocolateyPackageName
-  fileType      = 'exe'
-  url           = $url
-  softwareName  = 'autodesk desktop connector*'
-  checksum      = $checksum_url
-  checksumType  = 'sha256'
-  silentArgs    = "/quiet /norestart"
-  validExitCodes= @(0, 3010, 1641)
-}
+$checksum       = '0CEA70045727CF355927C42EA9AC2F9F44815AD9A3CF60BF200C3071C8AE2C68'
+$unzip           = Join-Path $env:TEMP 'Autodesk_Desktop_Connector_14_9_0_1430_Win_64bit'
 
-Install-ChocolateyPackage @packageArgsURL
+$packageArgsUnzip = @{
+  packageName    = 'Unzip'
+  fileType       = 'exe'
+  url            = $url
+  softwareName   = 'Unzip*'
+  checksum       = $checksum
+  checksumType   = 'sha256'
+  silentArgs     = "-suppresslaunch -d $env:TEMP"
+  validExitCodes = @(0, 3010, 1641)
+}
+Install-ChocolateyPackage @packageArgsUnzip
+
+$adsso           = Join-Path $unzip 'x64\AdSSO\AdSSO.msi'
+$packageArgsAdSSO  = @{
+  packageName    = 'Autodesk Single Sign On Component'
+  fileType       = 'msi'
+  file           = $adsso
+  softwareName   = 'Autodesk Single Sign On Component*'
+  silentArgs     = "/qn /norestart"
+  validExitCodes = @(0, 3010, 1641)
+}
+Install-ChocolateyInstallPackage @packageArgsAdSSO
+
+$cdx             = Join-Path $unzip 'x64\CDX\CDX.msi'
+$packageArgsCDX  = @{
+  packageName    = 'Autodesk Desktop Connector'
+  fileType       = 'msi'
+  file           = $cdx
+  softwareName   = 'Autodesk Desktop Connector*'
+  silentArgs     = '/qn /norestart'
+  validExitCodes = @(0, 3010, 1641)
+}
+Install-ChocolateyInstallPackage @packageArgsCDX
 
 #Close software running in elevated mode
 Stop-Process -Name "DesktopConnector*" -Force
