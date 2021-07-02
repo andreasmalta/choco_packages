4bd3b554154aee94f342cbf26a3be84ed6f6ffd8
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2021-07-02 10:31:36 +0200

    v4.15.0.12

--- clickshare-desktop/tools/chocolateyinstall.ps1
+++ clickshare-desktop/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$url            = "https://www.barco.com/services/website/en/TdeFiles/Download?FileNumber=R3306194&TdeType=3&MajorVersion=04&MinorVersion=14&PatchVersion=00&BuildVersion=10&ShowDownloadPage=False"
-$checksum_url   = '0978D7EB5E73249E5DFBA8D1F09B1EA4A25BEB454349094A3286C80BCF234005'
+$url            = "https://www.barco.com/services/website/en/TdeFiles/Download?FileNumber=R3306194&TdeType=3&MajorVersion=04&MinorVersion=15&PatchVersion=00&BuildVersion=012&ShowDownloadPage=False"
+$checksum_url   = '5A6DC9564A3881E548C3DA461B7611F0114A37E7B4F11461FFBB063297B962E6'
 $file           = Join-Path $env:TEMP 'ClickShare_Installer.msi'
 
 $packageArgsURL = @{
