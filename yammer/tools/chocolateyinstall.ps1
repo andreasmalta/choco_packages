6038639a6e62066c132de88597c108c804a87018
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2021-07-29 08:31:09 +0200

    v3.4.9

--- yammer/tools/chocolateyinstall.ps1
+++ yammer/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$url             = 'https://yammerdesktopapp.blob.core.windows.net/binaries/dist/win32/ia32/Yammer-ia32-3.4.8.msi'
-$checksum        = '279C05C788689447905F8D05134D4FB82D1910155116FB46DD3278F1A616B651'
+$url             = 'https://yammerdesktopapp.blob.core.windows.net/binaries/dist/win32/ia32/Yammer-ia32-3.4.9.msi'
+$checksum        = 'C275984AD0506BF8323CABED3C180585B5A2FB412DE37A4B13898C408CEF1F48'
 
-$url64           = 'https://yammerdesktopapp.blob.core.windows.net/binaries/dist/win32/x64/Yammer-x64-3.4.8.msi'
-$checksum64      = 'A6B7A2F41CC309E7D50F752F31C3DAE1D7F6A1E1E7A7FB4C691E5B713EE3332D'
+$url64           = 'https://yammerdesktopapp.blob.core.windows.net/binaries/dist/win32/x64/Yammer-x64-3.4.9.msi'
+$checksum64      = '28B7688E7BEB8D444EF9F3AA292065BAF16949E8AE33B9C89F76D8F732933149'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
