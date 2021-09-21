dd2d5226d18f210eb862f474d2c3c281504ddd56
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2021-09-21 21:45:22 +0200

    v3.9.0.0

--- kmupd/tools/chocolateyinstall.ps1
+++ kmupd/tools/chocolateyinstall.ps1
@@ -1,18 +1,23 @@
 ﻿$ErrorActionPreference = 'Stop';
 
+$FAX            = 'https://dl.konicaminolta.eu/en/?tx_kmanacondaimport_downloadproxy[fileId]=5d941dae7cb9d54a474cc91c8c4395a2&tx_kmanacondaimport_downloadproxy[documentId]=128824&tx_kmanacondaimport_downloadproxy[system]=KonicaMinolta&tx_kmanacondaimport_downloadproxy[language]=EN&type=1558521685'
+$FAX_checksum   = 'DB75C4511DD2641381D5CCA286CA2B4C7942AEE734B29E36CD7E11907061F71A'
 $PCL            = 'https://dl.konicaminolta.eu/en/?tx_kmanacondaimport_downloadproxy[fileId]=db69e8d121fea061d1d8558184cc2bc2&tx_kmanacondaimport_downloadproxy[documentId]=128825&tx_kmanacondaimport_downloadproxy[system]=KonicaMinolta&tx_kmanacondaimport_downloadproxy[language]=EN&type=1558521685'
 $PCL_checksum   = '85C5476F7671CC65D3864CB3C6829B0B497D216F2F9AC00B21E18879F2E8B9A9'
 $PS             = 'https://dl.konicaminolta.eu/en/?tx_kmanacondaimport_downloadproxy[fileId]=5ebe0f108582723d2feba8950d2b5fcc&tx_kmanacondaimport_downloadproxy[documentId]=128826&tx_kmanacondaimport_downloadproxy[system]=KonicaMinolta&tx_kmanacondaimport_downloadproxy[language]=EN&type=1558521685'
 $PS_checksum    = 'C453748DCC581EC85005E7E19163F424BBC358C02C718DF62A6C87260D6918A4'
 
+$FAX_extract    = Join-Path $env:TEMP 'extractFAX'
 $PCL_extract    = Join-Path $env:TEMP 'extractPCL'
 $PS_extract     = Join-Path $env:TEMP 'extractPS'
 
-$validExitCodes = @(0, 3010, 1605, 1614, 1641)
-Uninstall-ChocolateyPackage -PackageName "Universal PCL6 x64 Multi-Lingual driver" -FileType "msi" -SilentArgs "{710D3701-E7E6-4958-A75B-60663234694D} /qn /norestart" -ValidExitCodes $validExitCodes
-Uninstall-ChocolateyPackage -PackageName "Universal PS x64 Multi-Lingual driver" -FileType "msi" -SilentArgs "{8F32D344-DE7B-4D48-ADD0-1811AE858FFE} /qn /norestart" -ValidExitCodes $validExitCodes
-Uninstall-ChocolateyPackage -PackageName "Universal PCL5 x64 Multi-Lingual driver" -FileType "msi" -SilentArgs "{A8C9B8BA-88CA-4322-B686-9AD7330BCF06} /qn /norestart" -ValidExitCodes $validExitCodes
-
+$packageArgsFAX = @{
+	packageName   = $env:ChocolateyPackageName
+	unzipLocation = $FAX_extract
+	url           = $FAX
+	checksum      = $FAX_checksum
+	checksumType  = 'sha256'
+}
 $packageArgsPCL = @{
 	packageName   = $env:ChocolateyPackageName
 	unzipLocation = $PCL_extract
@@ -29,7 +34,15 @@ $packageArgsPS = @{
 }
 
 $pp = Get-PackageParameters
-if ($pp.'PCL' -or $pp.'PS') { 
+if ($pp.'FAX' -or $pp.'PCL' -or $pp.'PS') { 
+
+	if ($pp.'FAX') { 
+	Write-Host "INSTALLING FAX DRIVER"
+	Install-ChocolateyZipPackage @packageArgsFAX
+	Get-ChildItem $FAX_extract -Recurse -Filter "*.inf" | 
+	ForEach-Object { PNPUtil.exe /add-driver $_.FullName /install }
+	Add-PrinterDriver -Name "KONICA MINOLTA Universal FAX"
+	}
 
 	if ($pp.'PCL') { 
 	Write-Host "INSTALLING PCL DRIVER"
@@ -48,7 +61,13 @@ if ($pp.'PCL' -or $pp.'PS') {
 	}
 
 } else {
-	Write-Host "INSTALLING PCL/PS DRIVERS"
+	Write-Host "INSTALLING FAX/PCL/PS DRIVERS"
+
+	Install-ChocolateyZipPackage @packageArgsFAX
+	Get-ChildItem $FAX_extract -Recurse -Filter "*.inf" | 
+	ForEach-Object { PNPUtil.exe /add-driver $_.FullName /install }
+	Add-PrinterDriver -Name "KONICA MINOLTA Universal FAX"
+
 	Install-ChocolateyZipPackage @packageArgsPCL
 	Get-ChildItem $PCL_extract -Recurse -Filter "*.inf" | 
 	ForEach-Object { PNPUtil.exe /add-driver $_.FullName /install }
--- kmupd/tools/chocolateyuninstall.ps1
+++ kmupd/tools/chocolateyuninstall.ps1
@@ -1,2 +1,3 @@
-﻿Remove-PrinterDriver -Name "KONICA MINOLTA Universal PCL" -RemoveFromDriverStore
+﻿Remove-PrinterDriver -Name "KONICA MINOLTA Universal FAX" -RemoveFromDriverStore
+Remove-PrinterDriver -Name "KONICA MINOLTA Universal PCL" -RemoveFromDriverStore
 Remove-PrinterDriver -Name "KONICA MINOLTA Universal PS" -RemoveFromDriverStore
