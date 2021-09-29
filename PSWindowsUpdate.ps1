958c89f880ba814573616865cd2e94a216d34cca
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2021-09-29 10:24:41 +0200

    pswindowsupdate

--- PSWindowsUpdate.ps1
+++
@@ -1,5 +0,0 @@
-Install-PackageProvider -Name NuGet -Force
-Install-Module -Name PSWindowsUpdate -force
-Set-ExecutionPolicy RemoteSigned -force
-Import-Module -Name PSWindowsUpdate
-Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot
