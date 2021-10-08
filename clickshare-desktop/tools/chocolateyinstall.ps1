90fcf7b1d58da5aecf3f8456dbcb8e78b6211121
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2021-10-08 14:39:02 +0200

    v4.17.0.9

--- clickshare-desktop/tools/chocolateyinstall.ps1
+++ clickshare-desktop/tools/chocolateyinstall.ps1
@@ -1,5 +1,20 @@
 ﻿$ErrorActionPreference = 'Stop';
 
+#Uninstall old version before upgrade
+$DisplayName = 'ClickShare Desktop App Machine-Wide Installer'
+$ExitCodes = @(0, 3010, 1605, 1614, 1641)
+Get-ItemProperty -Path @('HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                         'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*') `
+                 -ErrorAction:SilentlyContinue `
+| Where-Object   {$_.DisplayName -like $DisplayName} `
+| ForEach-Object {
+	$silentArgs = '/qn /norestart'
+	$file = "$($_.UninstallString)"
+	$silentArgs = "$($_.PSChildName) $silentArgs"
+	$file = ''
+	Uninstall-ChocolateyPackage -PackageName "$DisplayName" -FileType "msi" -SilentArgs "$($silentArgs)" -File "$file" -ValidExitCodes $ExitCodes
+	}
+
 $url            = "https://www.barco.com/services/website/en/TdeFiles/Download?FileNumber=R3306194&TdeType=3&MajorVersion=04&MinorVersion=17&PatchVersion=00&BuildVersion=009&ShowDownloadPage=False"
 $checksum_url   = 'B6BD235B78F6F83F8BBA09C3E7557B219959EB0F85F3020DBE405E04D479FAA2'
 $file           = Join-Path $env:TEMP 'ClickShare_Installer.msi'
