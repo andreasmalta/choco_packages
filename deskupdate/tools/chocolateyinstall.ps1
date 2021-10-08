b0142e66898dbe6dfe74ea8ab31268ab20d1d6e3
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2021-10-08 12:21:39 +0200

    v5.02.0030.1

--- deskupdate/tools/chocolateyinstall.ps1
+++ deskupdate/tools/chocolateyinstall.ps1
@@ -1,5 +1,5 @@
 ﻿$ErrorActionPreference = 'Stop';
-$url             = 'https://webdownloads3.ts.fujitsu.com/download/FileDownload/fileDownload.aspx?SoftwareGUID=2A321609-0FBB-480D-B331-0968AA886CC1&FileFolder=Downloadfiles&FileTypeExtension=EXE&FileNameClient=FTS_DeskUpdateSetup_5020030_1257366.EXE'
+$url             = 'https://webdownloads.ts.fujitsu.com/download/FileDownload/fileDownload.aspx?SoftwareGUID=2A321609-0FBB-480D-B331-0968AA886CC1&FileFolder=Downloadfiles&FileTypeExtension=EXE&FileNameClient=FTS_DeskUpdateSetup_5020030_1257366.EXE'
 $checksum_url    = '260DA0BD1F9C1E6959256CC438D13E03C487479BAC05B4D0967EDCD16488FE3A'
 
 $packageArgs     = @{
