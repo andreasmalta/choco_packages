959c093156014d73e7dc4706fcc6d6e4f3284441
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2021-10-14 11:04:39 +0200

    v9.9.58.0

--- hpsupportassistant/tools/chocolateybeforemodify.ps1
+++ hpsupportassistant/tools/chocolateybeforemodify.ps1
@@ -1,33 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-#support assistant upgrade & uninstallation is not silent
-
-Get-AppxPackage *HPSupportAssistant* | Remove-AppxPackage -AllUsers
-#autohotkey to make silent
-#HP Support Assistant Uninstall - Keep Preferences?
-#Click the Yes button to keep your settings and preferences if you intend to install HP Support Assistant later
-#How do you want to open this?
-
-
 $folderlong = 'C:\Program Files (x86)\Hewlett-Packard\HP Support Framework\UninstallHPSA.exe'
 if (Test-path $folderlong) { 
-	$packageArgs = @{
-		packageName = $env:ChocolateyPackageName
-		fileType    = 'exe'
-		silentArgs  = '/s /v /qn'
-		file		= $folderlong
-	}
-Uninstall-ChocolateyPackage @packageArgs
+Uninstall-ChocolateyPackage -PackageName $env:ChocolateyPackageName -FileType 'exe' -SilentArgs '/s /v /qn' -File "$folderlong"
 }
 
 $foldershort = 'C:\Program Files (x86)\HP\HP Support Framework\UninstallHPSA.exe'
 if (Test-path $foldershort) { 
-	$packageArgs = @{
-		packageName	= $env:ChocolateyPackageName
-		fileType	= 'exe'
-		silentArgs	= '/s /v /qn'
-		file		= $foldershort
-	}
-Uninstall-ChocolateyPackage @packageArgs
+Uninstall-ChocolateyPackage -PackageName $env:ChocolateyPackageName -FileType 'exe' -SilentArgs '/s /v /qn' -File "$foldershort"
 }
-
