a3f216880b5a450aa6e313a3d420c5bff0604917
Andersson Andreas <Andersson.Andreas@voestalpine.com>
2021-10-27 15:51:22 +0200

    v1.8.0.0

--- hpdiagnostics/tools/chocolateyinstall.ps1
+++ hpdiagnostics/tools/chocolateyinstall.ps1
@@ -1,6 +1,21 @@
 ﻿$ErrorActionPreference = 'Stop';
-$url            = 'https://ftp.ext.hp.com/pub/softpaq/sp118001-118500/sp118349.exe'
-$checksum       = '3B25F9571CCBFE41CC15CF37AAC9DF42C1595E593FCF2AD5EDCF6C9ECF6797C7'
+$url            = 'https://ftp.ext.hp.com/pub/softpaq/sp135501-136000/sp135762.exe'
+$checksum       = '1962586DA8DFE9BA2D1542C2341E837759979DEBFE5F1CE85AAB645ED7927773'
+
+#Uninstall old version before upgrade
+$DisplayName = 'HP PC Hardware Diagnostics Windows'
+$ExitCodes = @(0, 3010, 1605, 1614, 1641)
+Get-ItemProperty -Path @('HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                         'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*') `
+                 -ErrorAction:SilentlyContinue `
+| Where-Object   {$_.DisplayName -like $DisplayName} `
+| ForEach-Object {
+	$silentArgs = '/qn /norestart'
+	$file = "$($_.UninstallString)"
+	$silentArgs = "$($_.PSChildName) $silentArgs"
+	$file = ''
+	Uninstall-ChocolateyPackage -PackageName "$DisplayName" -FileType "msi" -SilentArgs "$($silentArgs)" -File "$file" -ValidExitCodes $ExitCodes
+	}
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
