7d3d52cd6bd99282bb5295ba30baaf989e6104d0
Andersson Andreas <Andersson.Andreas@voestalpine.com>
2021-11-22 11:29:30 +0100

    Delete PSWindowsUpdate.cmd

--- PSWindowsUpdate.cmd
+++
@@ -1,1 +0,0 @@
-powershell.exe -Command "&{Install-PackageProvider -Name NuGet -Force; Install-Module -Name PSWindowsUpdate -force; Set-ExecutionPolicy RemoteSigned -force; Import-Module -Name PSWindowsUpdate; Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot}"
