4c51cffdb30e990fbc1b6c75b7a7f1990f7bc0e9
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2022-03-05 21:37:11 +0100

    v1.7.2

--- brightauthorconnected/tools/chocolateyinstall.ps1
+++ brightauthorconnected/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
-$url            = 'https://brightsignnetwork.com/download/nsingh/ba-connected/BA%20connected%20Setup%201.6.52.exe'
-$checksum_url   = 'E83DE4B0B0AD64AA10330B4F683B26E8ED0D3641AC1936CC274C60BE777B7F99'
+$url            = 'https://brightsignnetwork.com/download/nsingh/ba-connected/BA%20connected%20Setup%201.7.2.exe'
+$checksum_url   = '2B766980F2F9EF0183A5D0E0FA8F54F6B24CC38DAB47F51908B2AB8E99DB3F89'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
