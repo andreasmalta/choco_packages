0b9c9e517eb4894cdddeb3ccce6ca541db041dc7
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2022-03-14 20:45:54 +0100

    v9.14.42.0

--- hpsupportassistant/tools/chocolateyinstall.ps1
+++ hpsupportassistant/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
-$url            = 'https://ftp.ext.hp.com/pub/softpaq/sp138001-138500/sp138267.exe'
-$checksum_url   = 'D48D7A4631AE30107A670D937B74ED56C1B49C94B41F5A004C00FA9FD79CF2BC'
+$url            = 'https://ftp.ext.hp.com/pub/softpaq/sp138501-139000/sp138693.exe'
+$checksum_url   = 'BFAE798D945A845F9EE8BE06D95FF9C54966C5EDACFC3CFF4903515F9B326CD0'
 
 $packageArgsURL = @{
   packageName   = $env:ChocolateyPackageName
@@ -12,5 +12,5 @@ $packageArgsURL = @{
 Install-ChocolateyZipPackage @packageArgsURL
 
 #apparently needs to open in a new window otherwise it closes down the choco process (start-process -wait)
-#apparently needs to run from the same directory to work (cd $env:temp)
+#apparently needs to run from the same directory to work ($env:temp)
 cd $env:TEMP; Start-Process -Wait -FilePath ".\Setup.exe" -ArgumentList "-s"
