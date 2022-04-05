e560abaf1b32f101dde8d204579b0ec9be86493b
Andersson Andreas <Andersson.Andreas@voestalpine.com>
2022-04-05 14:24:02 +0200

    v2023.24.2.53

--- dwgtrueview/tools/chocolateyinstall.ps1
+++ dwgtrueview/tools/chocolateyinstall.ps1
@@ -25,24 +25,21 @@ if (Test-path $RegRebootRequired) { Remove-Item -Path $RegRebootRequired }
 $pp = Get-PackageParameters
 if ($pp.'French') {
 Write-Host "INSTALLING DWG TRUEVIEW - FRENCH"
-#$url = 'https://efulfillment.autodesk.com/NetSWDLD/2023/ACD/D40E729B-6243-3050-A267-3F664296250F/SFX/DWGTrueView_2023_French_64bit_dlm.sfx.exe'
-$url = 'Z:\dwgtrueview\DWGTrueView_2023_French_64bit_dlm.sfx.exe'
+$url = 'https://efulfillment.autodesk.com/NetSWDLD/2023/ACD/D40E729B-6243-3050-A267-3F664296250F/SFX/DWGTrueView_2023_French_64bit_dlm.sfx.exe'
 $checksum = 'EC47185FC42C5D8C86E607D0111977CC9EA8752DFB0EE1A462FDE6530FF4C05C'
 $unzip = Join-Path $env:TEMP 'DWGTrueView_2023_French_64bit_dlm\x64\dwgviewr\dwgviewr.msi'
 }
 
 elseif ($pp.'Japanese') {
 Write-Host "INSTALLING DWG TRUEVIEW - JAPANESE"
-#$url = 'https://efulfillment.autodesk.com/NetSWDLD/2023/ACD/B2459683-0F7B-36BD-84CB-3D821476766E/SFX/DWGTrueView_2023_Japanese_64bit_dlm.sfx.exe'
-$url = 'Z:\dwgtrueview\DWGTrueView_2023_Japanese_64bit_dlm.sfx.exe'
+$url = 'https://efulfillment.autodesk.com/NetSWDLD/2023/ACD/B2459683-0F7B-36BD-84CB-3D821476766E/SFX/DWGTrueView_2023_Japanese_64bit_dlm.sfx.exe'
 $checksum = '83A3E7F7B36741756A8DEB29924A5DA900133BB440177F3F6046C9D4A5DB95D8'
 $unzip = Join-Path $env:TEMP 'DWGTrueView_2023_Japanese_64bit_dlm\x64\dwgviewr\dwgviewr.msi'
 }
 
 else {
 Write-Host "INSTALLING DWG TRUEVIEW - ENGLISH"
-#$url = 'https://efulfillment.autodesk.com/NetSWDLD/2023/ACD/5E30049E-BFCC-3E1D-B280-6216BD413995/SFX/DWGTrueView_2023_English_64bit_dlm.sfx.exe'
-$url = 'Z:\dwgtrueview\DWGTrueView_2023_English_64bit_dlm.sfx.exe'
+$url = 'https://efulfillment.autodesk.com/NetSWDLD/2023/ACD/5E30049E-BFCC-3E1D-B280-6216BD413995/SFX/DWGTrueView_2023_English_64bit_dlm.sfx.exe'
 $checksum = '03AD6A7B69E97BEF719239658EDFB03A996919DF2763592FA35FFBF495BFF946'
 $unzip = Join-Path $env:TEMP 'DWGTrueView_2023_English_64bit_dlm\x64\dwgviewr\dwgviewr.msi'
 }
