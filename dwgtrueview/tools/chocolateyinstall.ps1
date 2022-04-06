029fd60c4d1b94a09dc3274665d82f91332c1a4b
Andersson Andreas <Andersson.Andreas@voestalpine.com>
2022-04-06 21:34:42 +0200

    v2023.24.2.53

--- dwgtrueview/tools/chocolateyinstall.ps1
+++ dwgtrueview/tools/chocolateyinstall.ps1
@@ -24,31 +24,28 @@ if (Test-path $RegRebootRequired) { Remove-Item -Path $RegRebootRequired }
 #INSTALLATION SETTINGS
 $pp = Get-PackageParameters
 if ($pp.'French') {
-Write-Host "INSTALLING DWG TRUEVIEW - FRENCH"
 $url = 'https://efulfillment.autodesk.com/NetSWDLD/2023/ACD/D40E729B-6243-3050-A267-3F664296250F/SFX/DWGTrueView_2023_French_64bit_dlm.sfx.exe'
 $checksum = 'EC47185FC42C5D8C86E607D0111977CC9EA8752DFB0EE1A462FDE6530FF4C05C'
-$unzip = Join-Path $env:TEMP 'DWGTrueView_2023_French_64bit_dlm\x64\dwgviewr\dwgviewr.msi'
+$file = Join-Path $env:TEMP 'DWGTrueView_2023_French_64bit_dlm\setup.exe'
 }
 
 elseif ($pp.'Japanese') {
-Write-Host "INSTALLING DWG TRUEVIEW - JAPANESE"
 $url = 'https://efulfillment.autodesk.com/NetSWDLD/2023/ACD/B2459683-0F7B-36BD-84CB-3D821476766E/SFX/DWGTrueView_2023_Japanese_64bit_dlm.sfx.exe'
 $checksum = '83A3E7F7B36741756A8DEB29924A5DA900133BB440177F3F6046C9D4A5DB95D8'
-$unzip = Join-Path $env:TEMP 'DWGTrueView_2023_Japanese_64bit_dlm\x64\dwgviewr\dwgviewr.msi'
+$file = Join-Path $env:TEMP 'DWGTrueView_2023_Japanese_64bit_dlm\setup.exe'
 }
 
 else {
-Write-Host "INSTALLING DWG TRUEVIEW - ENGLISH"
 $url = 'https://efulfillment.autodesk.com/NetSWDLD/2023/ACD/5E30049E-BFCC-3E1D-B280-6216BD413995/SFX/DWGTrueView_2023_English_64bit_dlm.sfx.exe'
 $checksum = '03AD6A7B69E97BEF719239658EDFB03A996919DF2763592FA35FFBF495BFF946'
-$unzip = Join-Path $env:TEMP 'DWGTrueView_2023_English_64bit_dlm\x64\dwgviewr\dwgviewr.msi'
+$file = Join-Path $env:TEMP 'DWGTrueView_2023_English_64bit_dlm\Setup.exe'
 }
 
 $packageArgsUnzip = @{
-  packageName    = $env:ChocolateyPackageName
+  packageName    = 'DWG TrueView Installation Files'
   fileType       = 'exe'
   url            = $url
-  softwareName   = 'dwgtrueview*'
+  softwareName   = 'DWG TrueView Installation Files*'
   checksum       = $checksum
   checksumType   = 'sha256'
   silentArgs     = "-suppresslaunch -d $env:TEMP"
@@ -56,25 +53,12 @@ $packageArgsUnzip = @{
 }
 Install-ChocolateyPackage @packageArgsUnzip
 
-$packageArgsMSI  = @{
-  packageName    = $env:ChocolateyPackageName
-  fileType       = 'msi'
-  file           = $unzip
-  softwareName   = 'dwgtrueview*'
-  silentArgs     = "/q ADSK_SETUP_EXE=1"
+$packageArgs  = @{
+  packageName    = 'DWG TrueView'
+  fileType       = 'exe'
+  file           = $file
+  softwareName   = 'DWG TrueView*'
+  silentArgs     = '-q'
   validExitCodes = @(0, 3010, 1641)
 }
-Install-ChocolateyInstallPackage @packageArgsMSI
-
-
-#CREATING MSI UNINSTALLER
-$packageName = 'DWG TrueView 2023*'
-Get-ItemProperty -Path @('HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
-                         'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*') `
-                 -ErrorAction:SilentlyContinue `
-| Where-Object   {$_.DisplayName -like $packageName} `
-| ForEach-Object {
-    Set-Itemproperty -path $_.PsPath -Name 'NoRemove' -value '0' -Type dword
-	Set-Itemproperty -path $_.PsPath -Name 'SystemComponent' -value '0' -Type dword
-	Set-ItemProperty -path $_.PsPath -name 'UninstallString' -value "MsiExec.exe /I$($_.PSChildName)" -Type ExpandString
-	}
+Install-ChocolateyInstallPackage @packageArgs
---
+++ dwgtrueview/tools/chocolateyuninstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop';
+
+#UNINSTALL
+$packageName = '*DWG TrueView*'
+$folderRoot = 'C:\Program Files\Autodesk'
+$validExitCodes = @(0, 3010, 1603, 1605, 1614, 1641)
+Get-ItemProperty -Path @('HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                         'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*') `
+                 -ErrorAction:SilentlyContinue `
+| Where-Object   {$_.DisplayName -like $packageName} `
+| ForEach-Object {
+	$silentArgs = "$($_.PSChildName) /qn /norestart"
+	if($($_.PSChildName) -like '{*') { Uninstall-ChocolateyPackage -PackageName "$($_.DisplayName)" -FileType "msi" -SilentArgs "$($silentArgs)" -File '' -ValidExitCodes $validExitCodes }
+	Remove-Item $_.PsPath -Recurse -ErrorAction Ignore
+	}
+if (Test-Path $folderRoot) { Get-ChildItem $folderRoot -Recurse -Force -Directory -Include $packageName | Remove-Item -Recurse -Confirm:$false -Force }
