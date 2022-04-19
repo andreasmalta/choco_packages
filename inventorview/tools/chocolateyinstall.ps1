a7b7c749795575b3e97a49bd4ca454a011e59188
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2022-04-19 22:26:42 +0200

    v2023.270158

--- inventorview/tools/chocolateyinstall.ps1
+++ inventorview/tools/chocolateyinstall.ps1
@@ -1,5 +1,30 @@
 ﻿$ErrorActionPreference = 'Stop';
 
+#UNINSTALL OLD VERSIONS
+$packageName = '*Inventor View*'
+$folderRoot = 'C:\Program Files\Autodesk'
+$validExitCodes = @(0, 3010, 1603, 1605, 1614, 1641)
+Get-ItemProperty -Path @('HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                         'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*') `
+                 -ErrorAction:SilentlyContinue `
+| Where-Object   {$_.DisplayName -like $packageName} `
+| ForEach-Object {
+	$silentArgs = "$($_.PSChildName) /qn /norestart"
+	if($($_.PSChildName) -like '{*') { Uninstall-ChocolateyPackage -PackageName "$($_.DisplayName)" -FileType "msi" -SilentArgs "$($silentArgs)" -File '' -ValidExitCodes $validExitCodes }
+	Remove-Item $_.PsPath -Recurse -ErrorAction Ignore
+	}
+if (Test-Path $folderRoot) { Get-ChildItem $folderRoot -Recurse -Force -Directory -Include $packageName | Remove-Item -Recurse -Confirm:$false -Force }
+
+
+#REMOVE REBOOT REQUESTS
+$RegRebootRequired = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired"
+if (Test-path $RegRebootRequired) { Remove-Item -Path $RegRebootRequired }
+
+
+#EXTRACT AND INSTALL
+$pp = Get-PackageParameters
+
+
 $url             = 'https://download.autodesk.com/us/support/files/autodesk_inventor_view_2022/inventor_view_2022_english_win_64bit_dlm.sfx.exe'
 $checksum        = '94C4E11509DEA6F7857B936920E627D27160EE2EA148B72CFCF1B48477B6C94B'
 $unzip           = Join-Path $env:TEMP 'Inventor_View_2022_English_Win_64bit_DLM'
