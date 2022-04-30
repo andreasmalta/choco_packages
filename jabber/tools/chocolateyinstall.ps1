523e6e601f1884636be82a0228cfcbae997a3a1f
Andersson Andreas <Andersson.Andreas@voestalpine.com>
2022-04-30 16:50:40 +0200

    jabber.14.1.1

--- jabber/tools/chocolateyinstall.ps1
+++ jabber/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$url            = 'https://binaries.webex.com/jabberclientwindows/20211230033337/CiscoJabberSetup.msi'
-$checksum_url   = 'AFB8156191B8CB47196B9BCAC218DF164D7DF59DE83C84D5B42CBCA3442C7E5A'
+$url            = 'https://binaries.webex.com/jabberclientwindows/20220428041046/CiscoJabberSetup.msi'
+$checksum_url   = '41E2C39FB6E405BEE8FC0667A9F7BF6109D78E0BD30F4836FDC95639F4F5AFB9'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
