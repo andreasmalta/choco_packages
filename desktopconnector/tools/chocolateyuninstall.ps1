1c90c4a0662df803a4e70f78be316639d8400dd2
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2022-05-07 20:47:06 +0200

    desktopconnector.15.6.0.1752

--- desktopconnector/tools/chocolateyuninstall.ps1
+++ desktopconnector/tools/chocolateyuninstall.ps1
@@ -1,19 +1,19 @@
-function Invoke-UninstallOldAutocad {
-$packageName = '*AutoCAD LT*'
-$packageName2 = '*Material Library*'
-$folderRoot = 'C:\Program Files\Autodesk'
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+. $toolsDir\helpers.ps1
+Invoke-UninstallOldAutodesk
+
+$packageName = 'Autodesk Single Sign On Component*'
+$FolderName = 'C:\Program Files\Autodesk\Autodesk AdSSO'
 $validExitCodes = @(0, 3010, 1603, 1605, 1614, 1641)
 Get-ItemProperty -Path @('HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
                          'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*') `
                  -ErrorAction:SilentlyContinue `
-| Where-Object   {$_.DisplayName -like $packageName -or $_.DisplayName -like $packageName2} `
+| Where-Object {$_.DisplayName -like $packageName} `
 | ForEach-Object {
 	$silentArgs = "$($_.PSChildName) /qn /norestart"
 	if($($_.PSChildName) -like '{*') { Uninstall-ChocolateyPackage -PackageName "$($_.DisplayName)" -FileType "msi" -SilentArgs "$($silentArgs)" -File '' -ValidExitCodes $validExitCodes }
 	Remove-Item $_.PsPath -Recurse -ErrorAction Ignore
 	}
-}
-
-in chocolateyinstall.ps1 & chocolateyuninstall.ps1
-. $toolsDir\helpers.ps1
-Invoke-UninstallOldAutocad
+if (Test-Path $FolderName) { Remove-Item $FolderName -Recurse -Force }
