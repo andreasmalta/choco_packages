c52936146317623ac1e1a7f894b7eb8f5fb9fe2f
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2022-05-07 17:02:57 +0200

    desktopconnector.15.6.0.1752

--- desktopconnector/tools/chocolateyinstall.ps1
+++ desktopconnector/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $url            = 'https://up.autodesk.com/2017/CDX/AB4AADCC-F890-4B4F-A7A6-B0FBD2386796/DesktopConnector-x64.exe'
-$checksum       = '04063CE003ED4EBC7524F34BFF804FB63B39713CD3E05C688A94356D608EDC48'
-$unzip           = Join-Path $env:TEMP 'Autodesk_Desktop_Connector_15_5_0_1684_Win_64bit'
+$checksum       = 'BD36E711A8270EBFAACD85EB5A30D65C5373B0AAA7D768DA1D39F18E63514028'
+$unzip           = Join-Path $env:TEMP 'Autodesk_Desktop_Connector_15_6_0_1752_Win_64bit'
 
 $packageArgsUnzip = @{
   packageName    = 'Unzip'
