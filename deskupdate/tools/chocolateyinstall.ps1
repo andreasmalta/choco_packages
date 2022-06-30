8d4e3f52f6d5d20667f57367787d51efed76efea
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2022-06-30 21:12:01 +0200

    deskupdate.5.02.0040

--- deskupdate/tools/chocolateyinstall.ps1
+++ deskupdate/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
-$url             = 'https://webdownloads2.ts.fujitsu.com/download/FileDownload/fileDownload.aspx?SoftwareGUID=81428530-BA47-4E87-813E-905E7230CCEC&FileFolder=Downloadfiles&FileTypeExtension=EXE&FileNameClient=FTS_DeskUpdateSetup_5020037_1257531.EXE'
-$checksum_url    = 'A47E19EB3B6A7CE218125812F5661E411E80935A8F821FB614A70416CEB4BDF0'
+$url             = 'https://webdownloads4.ts.fujitsu.com/download/FileDownload/fileDownload.aspx?SoftwareGUID=9105CDDF-323F-483B-8C80-B9A62182FA3B&FileFolder=Downloadfiles&FileTypeExtension=EXE&FileNameClient=FTS_DeskUpdateSetup_5020040_1268978.EXE'
+$checksum_url    = 'B46CB8E525FC63D777C52C11306640FB20F829D1200424EFD59DDD381F4258C3'
 
 $packageArgs     = @{
   packageName    = $env:ChocolateyPackageName
