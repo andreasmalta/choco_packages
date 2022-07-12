92ce4e0c6ea27fd73a252e6b25f8b3df1aa662cb
Andersson Andreas <Andersson.Andreas@voestalpine.com>
2022-07-12 11:45:20 +0200

    flashbackpro.5.56

---
+++ flashbackpro/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+$url = 'https://files.bbsoftware.co.uk/bbflbk5.exe'
+$checksum = '8118F7637CEF1A3D9E95F2B32F3D70D8ADC7B5597F584BB6658BC914AB90D4BB'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'exe'
+  url           = $url
+  softwareName  = 'flashbackpro*'
+  checksum      = $checksum
+  checksumType  = 'sha256'
+  silentArgs    = "/S"
+  validExitCodes= @(0, 3010, 1641)
+}
+Install-ChocolateyPackage @packageArgs
---
+++ flashbackpro/tools/chocolateyuninstall.ps1
@@ -0,0 +1,5 @@
+﻿$uninstall64 = "C:\Program Files (x86)\Blueberry Software\FlashBack Pro 5\uninstall.exe"
+if (Test-path $uninstall64) { Uninstall-ChocolateyPackage -PackageName "FlashBack Pro 5" -FileType "exe" -SilentArgs "/S" -File $uninstall64 }
+
+$uninstall32 = "C:\Program Files\Blueberry Software\FlashBack Pro 5\uninstall.exe"
+if (Test-path $uninstall32) { Uninstall-ChocolateyPackage -PackageName "FlashBack Pro 5" -FileType "exe" -SilentArgs "/S" -File $uninstall32 }
