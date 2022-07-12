7ac175b4c5d1a5f4d01170d1f58fc6dc9b4d7173
Andersson Andreas <Andersson.Andreas@voestalpine.com>
2022-07-12 11:28:39 +0200

    fbx.3.18

---
+++ fbx/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+$url = 'https://files.bbsoftware.co.uk/bbfbx.exe'
+$checksum = 'D331F6739C6A54ED48E1E1638E838B5BF45BCF01C5127CB68504F13F7741F48C'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'exe'
+  url           = $url
+  softwareName  = 'fbx*'
+  checksum      = $checksum
+  checksumType  = 'sha256'
+  silentArgs    = "/S"
+  validExitCodes= @(0, 3010, 1641)
+}
+Install-ChocolateyPackage @packageArgs
---
+++ fbx/tools/chocolateyuninstall.ps1
@@ -0,0 +1,4 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$uninstall = "C:\Program Files\Blueberry Software\FBX Game Recorder\uninstall.exe"
+if (Test-path $uninstall) { Uninstall-ChocolateyPackage -PackageName "FBX Game Recorder" -FileType "exe" -SilentArgs "/S" -File $uninstall }
