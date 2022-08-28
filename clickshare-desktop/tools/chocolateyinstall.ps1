37ed0047bee26c454df88b94316e72256b39f4bd
Andersson Andreas <Andersson.Andreas@voestalpine.com>
2022-08-28 12:03:34 +0200

    clickshare-desktop.4.23.0.23

--- clickshare-desktop/tools/chocolateyinstall.ps1
+++ clickshare-desktop/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 . $toolsDir\helpers.ps1
 Invoke-UninstallOldClickShare
 
-$url            = "https://www.barco.com/services/website/en/TdeFiles/Download?FileNumber=R3306194&TdeType=3&MajorVersion=04&MinorVersion=22&PatchVersion=00&BuildVersion=013&ShowDownloadPage=False"
-$checksum_url   = '7F44E361AD323660D994AB0A8A156D5241F7D05D12ACD849FB7291755FF0A213'
+$url            = "https://www.barco.com/services/website/en/TdeFiles/Download?FileNumber=R3306194&TdeType=3&MajorVersion=04&MinorVersion=23&PatchVersion=00&BuildVersion=023&ShowDownloadPage=False"
+$checksum_url   = '1582C62F464E41FA8B89768D1D79CC5D466AA60EDFEBA1810FB2211A95C68A2A'
 $file           = Join-Path $env:TEMP 'ClickShare_Installer.msi'
 
 $packageArgsURL = @{
