52e7c5eb08566965817e5c0e1d9ac25065c40752
Andersson Andreas <Andersson.Andreas@voestalpine.com>
2022-08-30 11:45:45 +0200

    desktopconnector.15.8.0.1827

--- desktopconnector/tools/chocolateyinstall.ps1
+++ desktopconnector/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url = 'https://up.autodesk.com/2017/CDX/AB4AADCC-F890-4B4F-A7A6-B0FBD2386796/DesktopConnector-x64-15.7.1.1780.exe'
-$checksum = '8E1B8462E07F70274C323C5908FBBE9B20B187F256591443E31D75B641B8F630'
-$file = Join-Path $env:TEMP 'Autodesk_Desktop_Connector_15_7_1_1780_Win_64bit\Setup.exe'
+$url = 'https://up.autodesk.com/2017/CDX/AB4AADCC-F890-4B4F-A7A6-B0FBD2386796/DesktopConnector-x64-15.8.0.1827.exe'
+$checksum = '00C0FA7E76DF12B249C3D048FA8C9183A96074E2FE1BCC6A164DAFB1E07B2858'
+$file = Join-Path $env:TEMP 'Autodesk_Desktop_Connector_15_8_0_1827_Win_64bit\Setup.exe'
 
 . $toolsDir\helpers.ps1
 Invoke-UninstallOldAutodesk
