e58d0f3ccf319b74d862038be7f1a96c56504af5
Andersson Andreas <Andersson.Andreas@voestalpine.com>
2022-10-07 08:33:55 +0200

    moldflowc.2023.0

--- moldflowc/tools/chocolateyinstall.ps1
+++ moldflowc/tools/chocolateyinstall.ps1
@@ -1,34 +1,38 @@
 ﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url			= 'https://damassets.autodesk.net/content/dam/autodesk/www/products/moldflow/files/MFC_2021_FCS_Multilingual_Win_64bit_dlm.sfx.exe'
-$checksum		= 'D8483B7A1CAB8178E2994734A117FD373FF6F393792091F7C751EB49721BAD2A'
-$file           = Join-Path $env:TEMP 'MFC_2021_FCS_Multilingual_Win_64bit_dlm\setup.exe'
+#ADJUST FOR LATEST VERSION
+$url = 'https://damassets.autodesk.net/content/dam/autodesk/www/products/moldflow/files/MFC_2023_FCS_Multilingual_Win_64bit_dlm.sfx.exe'
+$checksum = '86AA9230E79D7C322F821A48FFFE27FF6D054BF2D9757E0353E7DF0D3E24DCD6'
+$file = Join-Path $env:TEMP 'MFC_2023_FCS_Multilingual_Win_64bit_dlm\setup.exe'
 
-#Uninstall 2021.1
-$packagename     = 'Autodesk Moldflow Communicator 2021.1'
-$productcode     = '{753C779D-0564-4042-8000-8FF1C120CB5E}'
-$regkey          = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\$productcode"
-if (Test-path $regkey) { Uninstall-ChocolateyPackage -PackageName "$packagename" -FileType "msi" -SilentArgs "$productcode /qn /norestart" }
+#REMOVE OLD VERSIONS
+. $toolsDir\helpers.ps1
+Invoke-UninstallOldMFC
 
-$packageArgsURL = @{
-  packageName   = $env:ChocolateyPackageName
-  fileType      = 'exe'
-  url           = $url
-  softwareName  = 'autodesk moldflow communicator*'
-  checksum      = $checksum
-  checksumType  = 'sha256'
-  silentArgs    = "-suppresslaunch -d $env:TEMP"
-  validExitCodes= @(0, 3010, 1641)
+#REMOVE REBOOT REQUESTS
+$RegRebootRequired = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired"
+if (Test-path $RegRebootRequired) { Remove-Item -Path $RegRebootRequired }
+
+#EXTRACT & INSTALL
+$packageArgsUnzip = @{
+  packageName    = 'MFC Installation Files'
+  fileType       = 'exe'
+  url            = $url
+  softwareName   = 'MFC Installation Files*'
+  checksum       = $checksum
+  checksumType   = 'sha256'
+  silentArgs     = "-suppresslaunch -d $env:TEMP"
+  validExitCodes = @(0, 3010, 1641)
 }
+Install-ChocolateyPackage @packageArgsUnzip
 
-$packageArgsFile = @{
-  packageName   = $env:ChocolateyPackageName
-  fileType      = 'exe'
-  file          = $file
-  softwareName  = 'autodesk moldflow communicator*'
-  silentArgs    = "/q /w"
-  validExitCodes= @(0, 3010, 1641)
+$packageArgs  = @{
+  packageName    = 'Moldflow Communicator'
+  fileType       = 'exe'
+  file           = $file
+  softwareName   = 'Moldflow Communicator*'
+  silentArgs     = '-q'
+  validExitCodes = @(0, 3010, 1641)
 }
-
-Install-ChocolateyPackage @packageArgsURL
-Install-ChocolateyInstallPackage @packageArgsFile
+Install-ChocolateyInstallPackage @packageArgs
--- moldflowc/tools/chocolateyuninstall.ps1
+++ moldflowc/tools/chocolateyuninstall.ps1
@@ -1,17 +1,14 @@
-#Uninstall 2021.1
-$packagename     = 'Autodesk Moldflow Communicator 2021.1'
-$productcode     = '{753C779D-0564-4042-8000-8FF1C120CB5E}'
-$regkey          = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\$productcode"
-if (Test-path $regkey) { Uninstall-ChocolateyPackage -PackageName "$packagename" -FileType "msi" -SilentArgs "$productcode /qn /norestart" }
+$ErrorActionPreference = 'Stop';
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-#Uninstall 2021.2
-$packagename     = 'Autodesk Moldflow Communicator 2021.2'
-$productcode     = '{753C779D-0564-4044-8000-8FF1C120CB5E}'
-$regkey          = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\$productcode"
-if (Test-path $regkey) { Uninstall-ChocolateyPackage -PackageName "$packagename" -FileType "msi" -SilentArgs "$productcode /qn /norestart" }
+#UNINSTALL
+. $toolsDir\helpers.ps1
+Invoke-UninstallOldMFC
 
-#Uninstall Autodesk Desktop App
-Stop-Process -Name "Autodesk*" -Force
-Stop-Service -Name "AdAppMgrSvc"
-Remove-Item –path C:\ProgramData\Autodesk\SDS –recurse
-& "C:\Program Files (x86)\Autodesk\Autodesk Desktop App\removeAdAppMgr.exe" --mode unattended
+$uninstaller = 'C:\Program Files (x86)\Autodesk\Autodesk Desktop App\removeAdAppMgr.exe'
+if (Test-Path $uninstaller) {
+		Stop-Process -Name "Autodesk*" -Force
+		Stop-Service -Name "AdAppMgrSvc"
+		Remove-Item -Path 'C:\ProgramData\Autodesk\SDS' -recurse
+		Uninstall-ChocolateyPackage -PackageName 'Autodesk Desktop App' -FileType 'exe' -SilentArgs '--mode unattended' -File $uninstaller
+	}
