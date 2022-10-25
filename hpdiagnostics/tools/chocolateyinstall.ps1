a2b11a90978bdd3e742c6301108c05f5167a7b11
Andersson Andreas <Andersson.Andreas@voestalpine.com>
2022-10-25 11:57:16 +0200

    hpdiagnostics.1.8.3.0

--- hpdiagnostics/tools/chocolateyinstall.ps1
+++ hpdiagnostics/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $url = 'https://ftp.ext.hp.com/pub/softpaq/sp142501-143000/sp142974.exe'
-$checksum = 'FFC66FA68A14D1B359C1552956138165B1323D520A8A13EE8B881DE4E5529AF0'
+$checksum = 'FE6F4BE9AF781AED3A9A161FFFCDBC27C6EFC6AE3F0FAADCBCB10493FC0B7F8B'
 
 #Uninstall old version before upgrade
 . $toolsDir\helpers.ps1
