9bed4b20b18b1a973dc9910a43a7401d1cfc037d
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2022-12-22 16:18:39 +0100

    supportassist.3.13.0.236.nupkg

--- supportassist/tools/chocolateyinstall.ps1
+++ supportassist/tools/chocolateyinstall.ps1
@@ -1,15 +1,15 @@
 ﻿$ErrorActionPreference = 'Stop';
-$url = 'https://downloads.dell.com/serviceability/catalog/SupportAssistInstaller.exe'
-$checksum = '128161E2E24AA7C42528C8D9A32F67BD466276B2CC8E970F17F6C82FB88C5D1F'
+$url = "https://downloads.dell.com/serviceability/catalog/SupportAssistx64-" + $env:chocolateyPackageVersion + ".msi"
+$checksum = '939903E71C88261608730894FAE3DA9303EB4FDC81CE4EFB275069DA84223B72'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  fileType      = 'exe'
+  fileType      = 'msi'
   url           = $url
-  softwareName  = 'SupportAssist*'
   checksum      = $checksum
   checksumType  = 'sha256'
-  silentArgs    = "/S"
+  softwareName  = 'SupportAssist*'
+  silentArgs    = "/qn /norestart"
   validExitCodes= @(0, 3010, 1641)
 }
 Install-ChocolateyPackage @packageArgs
