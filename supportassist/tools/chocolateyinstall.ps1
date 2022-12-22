db160f9a6e7f223402c62d850e7132274524d12f
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2022-12-22 19:21:44 +0100

    supportassist.3.13.0.236

--- supportassist/tools/chocolateyinstall.ps1
+++ supportassist/tools/chocolateyinstall.ps1
@@ -1,15 +1,15 @@
 ﻿$ErrorActionPreference = 'Stop';
-$url = "https://downloads.dell.com/serviceability/catalog/SupportAssistx64-" + $env:chocolateyPackageVersion + ".msi"
-$checksum = '939903E71C88261608730894FAE3DA9303EB4FDC81CE4EFB275069DA84223B72'
+$url = 'https://downloads.dell.com/serviceability/catalog/SupportAssistInstaller.exe'
+$checksum = 'C036334A0730C13EE11FD79C6A555162160E79F10B7A82704D4C3553DF458569'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  fileType      = 'msi'
+  fileType      = 'exe'
   url           = $url
+  softwareName  = 'SupportAssist*'
   checksum      = $checksum
   checksumType  = 'sha256'
-  softwareName  = 'SupportAssist*'
-  silentArgs    = "/qn /norestart"
+  silentArgs    = "/S"
   validExitCodes= @(0, 3010, 1641)
 }
 Install-ChocolateyPackage @packageArgs
