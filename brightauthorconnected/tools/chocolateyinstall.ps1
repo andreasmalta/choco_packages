6ef273981d1fb9fa5c927be10e5e410b4cdafe36
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2023-03-03 16:32:08 +0100

    brightauthorconnected.1.8.3

--- brightauthorconnected/tools/chocolateyinstall.ps1
+++ brightauthorconnected/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
-$url = 'https://www.brightsignnetwork.com/download/ba-connected-builds/ba-connected-prod/BA%20connected%20Setup%201.7.17.exe'
-$checksum = 'DDF11ADB9CC286CF38C11CA0CD21021AA7D782B87D0D1120E55E56D7A9A5D822'
+$url = 'https://www.brightsignnetwork.com/download/ba-connected-builds/ba-connected-prod/BA%20connected%20Setup%201.8.3.exe'
+$checksum = '973A640EBD6F91A3587E730759FBCBC4DE08E1BB9859598E660C3A137CDAF8DF'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
