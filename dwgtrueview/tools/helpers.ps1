e9070656920efb99dc2f26d39d75e995568f6125
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2023-04-13 07:18:52 +0200

    dwgtrueview.2024.24.3.61

--- dwgtrueview/tools/helpers.ps1
+++ dwgtrueview/tools/helpers.ps1
@@ -1,6 +1,7 @@
 function Invoke-UninstallOldTrueView {
 $packageName = '*DWG TrueView*'
 $folderRoot = 'C:\Program Files\Autodesk'
+$startmenu = 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs'
 $validExitCodes = @(0, 3010, 1603, 1605, 1614, 1641)
 Get-ItemProperty -Path @('HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
                          'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*') `
@@ -12,4 +13,5 @@ Get-ItemProperty -Path @('HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVe
 	Remove-Item $_.PsPath -Recurse -ErrorAction Ignore
 	}
 if (Test-Path $folderRoot) { Get-ChildItem $folderRoot -Recurse -Force -Directory -Include $packageName | Remove-Item -Recurse -Confirm:$false -Force }
+Get-ChildItem $startmenu -Recurse -Force -Include $packageName | Remove-Item -Recurse -Confirm:$false -Force
 }
