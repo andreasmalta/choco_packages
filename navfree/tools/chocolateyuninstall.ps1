bebeacac001ff8e43e2ab06a974bbf961bda01ce
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2023-04-23 15:03:10 +0200

    navfree.2024.21.0.139716

--- navfree/tools/chocolateyuninstall.ps1
+++ navfree/tools/chocolateyuninstall.ps1
@@ -5,20 +5,9 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 . $toolsDir\helpers.ps1
 Invoke-UninstallOld
 
-#Remove Autodesk Identity
-$packageName = 'Autodesk Identity*'
-$validExitCodes = @(0, 3010, 1603, 1605, 1614, 1641)
-$AdskIdentity = 'C:\Program Files\Autodesk\AdskIdentityManager\uninstall.exe'
-Get-ItemProperty -Path @('HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
-                         'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*') `
-                 -ErrorAction:SilentlyContinue `
-| Where-Object   {$_.DisplayName -like $packageName} `
-| ForEach-Object {
-	$silentArgs = "$($_.PSChildName) /qn /norestart"
-	if($($_.PSChildName) -like '{*') { Uninstall-ChocolateyPackage -PackageName "$($_.DisplayName)" -FileType "msi" -SilentArgs "$($silentArgs)" -File '' -ValidExitCodes $validExitCodes }
-	Remove-Item $_.PsPath -Recurse -ErrorAction Ignore
-	}
-if (Test-Path $AdskIdentity) { Uninstall-ChocolateyPackage -PackageName 'Autodesk Identity' -FileType 'exe' -SilentArgs '--mode unattended' -File $AdskIdentity }
+#Remove Autodesk Identity Manager
+$AdskIdentityManager = 'C:\Program Files\Autodesk\AdskIdentityManager\uninstall.exe'
+if (Test-Path $AdskIdentityManager) { Uninstall-ChocolateyPackage -PackageName 'Autodesk Identity Manager' -FileType 'exe' -SilentArgs '--mode unattended' -File $uninstall }
 
 #Remove Autodesk Installer
 Get-Process "AdskAccessCore*" -ErrorAction SilentlyContinue | Stop-Process -Force
