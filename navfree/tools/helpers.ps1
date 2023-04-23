556781c3518c13240858f8b2b5f4cfec4185485f
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2023-04-23 07:00:29 +0200

    navfree.2024.21.0.139716

--- navfree/tools/helpers.ps1
+++ navfree/tools/helpers.ps1
@@ -9,14 +9,13 @@ Remove-Item -Path $RegRebootRequired -ErrorAction SilentlyContinue
 
 #Remove old versions
 $packageName = '*Navisworks Freedom*'
-$packageName2 = '*Material Library*'
 $folderRoot = 'C:\Program Files\Autodesk'
 $startmenu = 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs'
 $validExitCodes = @(0, 3010, 1603, 1605, 1614, 1641)
 Get-ItemProperty -Path @('HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
                          'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*') `
                  -ErrorAction:SilentlyContinue `
-| Where-Object   {$_.DisplayName -like $packageName -or $_.DisplayName -like $packageName2} `
+| Where-Object   {$_.DisplayName -like $packageName} `
 | ForEach-Object {
 	$silentArgs = "$($_.PSChildName) /qn /norestart"
 	if($($_.PSChildName) -like '{*') { Uninstall-ChocolateyPackage -PackageName "$($_.DisplayName)" -FileType "msi" -SilentArgs "$($silentArgs)" -File '' -ValidExitCodes $validExitCodes }
