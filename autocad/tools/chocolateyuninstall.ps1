fdcf8044504c27a23ef99c1ba0caf6a361648ed7
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2023-04-24 19:29:44 +0200

    autocad.2024.24.3.610

--- autocad/tools/chocolateyuninstall.ps1
+++ autocad/tools/chocolateyuninstall.ps1
@@ -1,19 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
+#UNINSTALL MAIN SOFTWARE
 . $toolsDir\helpers.ps1
 Invoke-UninstallOld
 
-#Remove Licensing app
-$AdskLicensing = 'C:\Program Files (x86)\Common Files\Autodesk Shared\AdskLicensing\uninstall.exe'
-if (Test-Path $AdskLicensing) { Uninstall-ChocolateyPackage -PackageName 'Autodesk Licensing' -FileType 'exe' -SilentArgs '--mode unattended' -File $AdskLicensing }
-
-#Remove the rest
+#Remove some remaining packages
 $packageName = 'Autodesk App*'
-$packageName2 = 'Autodesk Identity*'
 $packageName3 = 'Autodesk Featured*'
 $packageName4 = 'Autodesk Save*'
-$packageName5 = 'Autodesk Single*'
 $validExitCodes = @(0, 3010, 1603, 1605, 1614, 1641)
 Get-ItemProperty -Path @('HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
                          'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*') `
@@ -25,11 +20,25 @@ Get-ItemProperty -Path @('HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVe
 	Remove-Item $_.PsPath -Recurse -ErrorAction Ignore
 	}
 
-$packageName6 = 'Autodesk Genuine*'
+#Remove Autodesk Identity Manager
+$AdskIdentityManager = 'C:\Program Files\Autodesk\AdskIdentityManager\uninstall.exe'
+if (Test-Path $AdskIdentityManager) { Uninstall-ChocolateyPackage -PackageName 'Autodesk Identity Manager' -FileType 'exe' -SilentArgs '--mode unattended' -File $AdskIdentityManager }
+
+#Remove Autodesk Licensing
+$AdskLicensing = 'C:\Program Files (x86)\Common Files\Autodesk Shared\AdskLicensing\uninstall.exe'
+if (Test-Path $AdskLicensing) { Uninstall-ChocolateyPackage -PackageName 'Autodesk Licensing' -FileType 'exe' -SilentArgs '--mode unattended' -File $AdskLicensing }
+
+#Remove Autodesk Installer
+Get-Process "AdskAccessCore*" -ErrorAction SilentlyContinue | Stop-Process -Force
+$AdODIS = 'C:\Program Files\Autodesk\AdODIS\V1\RemoveODIS.exe'
+if (Test-Path $AdODIS) { Uninstall-ChocolateyPackage -PackageName 'Autodesk Installer' -FileType 'exe' -SilentArgs '--mode unattended' -File $AdODIS }
+
+#Remove Autodesk Genuine Service
+$packageName = 'Autodesk Genuine*'
 Get-ItemProperty -Path @('HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
                          'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*') `
                  -ErrorAction:SilentlyContinue `
-| Where-Object   {$_.DisplayName -like $packageName6} `
+| Where-Object   {$_.DisplayName -like $packageName} `
 | ForEach-Object {
 	$silentArgs = "$($_.PSChildName) /qn /norestart"
 	if($($_.PSChildName) -like '{*') { Uninstall-ChocolateyPackage -PackageName "$($_.DisplayName)" -FileType "msi" -SilentArgs "$($silentArgs)" -File '' -ValidExitCodes $validExitCodes }
