efdeb760c20eb0ae5054a5e3870cc1184383c816
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2023-09-08 23:02:13 +0200

    supportassist.3.14.0.91

--- supportassist/tools/chocolateyinstall.ps1
+++ supportassist/tools/chocolateyinstall.ps1
@@ -1,14 +1,28 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$url = "https://downloads.dell.com/serviceability/catalog/SupportAssistx64-" + $Env:ChocolateyPackageVersion + ".msi"
-$checksum = '2C3AA11080F1E90874A0F8DFE6ECEBB238B8D62F79B0D0A2A7757C750F53C49C'
+$url_msi = "https://downloads.dell.com/serviceability/catalog/SupportAssistx64-" + $Env:ChocolateyPackageVersion + ".msi"
+$checksum_msi = '2C3AA11080F1E90874A0F8DFE6ECEBB238B8D62F79B0D0A2A7757C750F53C49C'
+$url_msp = "https://downloads.dell.com/serviceability/catalog/SupportAssistx64-" + $Env:ChocolateyPackageVersion + ".msp"
+$checksum_msp = '024529C760808A73EAC86FAB587332C6689E4505CAD325F19932C9E998FC363A'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'msi'
-  url           = $url
+  url           = $url_msi
   softwareName  = 'SupportAssist*'
-  checksum      = $checksum
+  checksum      = $checksum_msi
+  checksumType  = 'sha256'
+  silentArgs     = "/qn /norestart"
+  validExitCodes= @(0, 3010, 1641)
+}
+Install-ChocolateyPackage @packageArgs
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'msp'
+  url           = $url_msp
+  softwareName  = 'SupportAssist*'
+  checksum      = $checksum_msp
   checksumType  = 'sha256'
   silentArgs     = "/qn /norestart"
   validExitCodes= @(0, 3010, 1641)
