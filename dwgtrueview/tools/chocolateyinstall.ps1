bd6af902a000116c11e7799ddc6dd4ef9fa60c49
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2024-04-12 21:09:38 +0200

    v2025

--- dwgtrueview/tools/chocolateyinstall.ps1
+++ dwgtrueview/tools/chocolateyinstall.ps1
@@ -5,47 +5,33 @@ $downloadsPath = (New-Object -ComObject Shell.Application).Namespace('shell:Down
 #1 LANGUAGE SELECTION
 $pp = Get-PackageParameters
 if ($pp.'French') {
-$url = 'https://upload1.delivery.autodesk.com/PORTAL_DownloadPackage301407108829295.exe?response-content-disposition=attachment%3B%20filename%20%3D%22Create_Installer_PLC0000037_2025_French_WIN64.exe%22&'
-$checksum = 'B69C16C885792279587410CD9A657B3A8FA81D9CCEE94BFF7515611942B4B11F'
-$file = Join-Path $downloadsPath 'Autodesk\DWG TrueView 2025 - Français (French) - (FR)\Setup.exe'
+$url = 'https://cdn.jsdelivr.net/gh/andreasmalta/choco_packages@master/dwgtrueview/Autodesk_DWG_TrueView_2025_fr-FR_setup_webinstall.exe'
+$checksum = 'D7CB1D080408C55DB49A510345F6ABD409ECFA0C166284019E441EC35C6CD3CF'
 }
 
 elseif ($pp.'Japanese') {
-$url = 'https://upload1.delivery.autodesk.com/PORTAL_DownloadPackage301460557392996.exe?response-content-disposition=attachment%3B%20filename%20%3D%22Create_Installer_PLC0000037_2025_Japanese_WIN64.exe%22&'
-$checksum = '444BDE2C1E35AA5892FE758A77CA0B518AB0CD96CF01A01DE41655472B260F1D'
-$file = Join-Path $downloadsPath 'Autodesk\DWG TrueView 2025 - 日本語(Japanese) - (JA)\Setup.exe'
+$url = 'https://cdn.jsdelivr.net/gh/andreasmalta/choco_packages@master/dwgtrueview/Autodesk_DWG_TrueView_2025_ja-JP_setup_webinstall.exe'
+$checksum = 'D328C2D5E63D483E9D4D5D0EF658124DBED51BD083B3F01A5FF5D56CC8947BF5'
 }
 
 else {
-$url = 'https://upload1.delivery.autodesk.com/PORTAL_DownloadPackage811010659915909.exe?response-content-disposition=attachment%3B%20filename%20%3D%22Create_Installer_PLC0000037_2025_English_WIN64.exe%22&'
-$checksum = '89FEE1F0F66ADB825B945850CDEA7FD4C02832852DF67578C28CE5F370B6A821'
-$file = Join-Path $downloadsPath 'Autodesk\DWG TrueView 2025 - English - (EN)\Setup.exe'
+$url = 'https://cdn.jsdelivr.net/gh/andreasmalta/choco_packages@master/dwgtrueview/Autodesk_DWG_TrueView_2025_en-US_setup_webinstall.exe'
+$checksum = '1145E637E55C6C7611FFCBA3CF080CB880B66EC7219E2334EDF9FF274610C2D7'
 }
 
-#2 DOWNLOAD
+#2 UNINSTALL OLD
+. $toolsDir\helpers.ps1
+Invoke-UninstallDWGTrueView
+
+#3 INSTALL
 $packageArgsDownload = @{
-  packageName    = 'DWG TrueView Installation Files'
+  packageName    = 'DWG TrueView'
   fileType       = 'exe'
   url            = $url
-  softwareName   = 'DWG TrueView Installation Files*'
+  softwareName   = 'DWG TrueView*'
   checksum       = $checksum
   checksumType   = 'sha256'
   silentArgs     = "-q"
   validExitCodes = @(0, 3010, 1641)
 }
 Install-ChocolateyPackage @packageArgsDownload
-
-#3 UNINSTALL OLD
-. $toolsDir\helpers.ps1
-Invoke-UninstallDWGTrueView
-
-#4 INSTALL
-$packageArgs  = @{
-  packageName    = 'DWG TrueView'
-  fileType       = 'exe'
-  file           = $file
-  softwareName   = 'DWG TrueView*'
-  silentArgs     = '-q'
-  validExitCodes = @(0, 3010, 1641)
-}
-Install-ChocolateyInstallPackage @packageArgs
