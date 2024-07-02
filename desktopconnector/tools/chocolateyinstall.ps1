6e0d62f790069eec84afd68c72c7301205a99446
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2024-07-02 10:40:08 +0200

    desktopconnector.16.9.1.2222

--- desktopconnector/tools/chocolateyinstall.ps1
+++ desktopconnector/tools/chocolateyinstall.ps1
@@ -2,36 +2,22 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $url = "https://up.autodesk.com/2017/CDX/AB4AADCC-F890-4B4F-A7A6-B0FBD2386796/DesktopConnector-x64-" + $Env:ChocolateyPackageVersion + ".exe"
-$checksum = '8293CF52DFBCE82665331EF44610BCBF6B325327BE6C7A89F280DDDA941B70E3'
+$checksum = '8DBA9030A752DD16733B9B318017D828D3A7215A6052562163FB2E43D3B5BDF7'
 
 . $toolsDir\helpers.ps1
-Invoke-UninstallOld
+Invoke-Uninstall15
 
-$packageArgsUnzip = @{
-  packageName    = 'Autodesk Desktop Connector Installation Files'
-  fileType       = 'exe'
-  url            = $url
-  softwareName   = 'Autodesk Desktop Connector Installation Files*'
-  checksum       = $checksum
-  checksumType   = 'sha256'
-  silentArgs     = "-suppresslaunch -d $env:TEMP"
-  validExitCodes = @(0, 3010, 1641)
-}
-Install-ChocolateyPackage @packageArgsUnzip
-
-$version = $Env:ChocolateyPackageVersion.replace('.','_')
-$folder = "Autodesk_Desktop_Connector_" + $version + "_Win_64bit\Setup.exe"
-$file = Join-Path $env:TEMP $folder
-
-$packageArgs  = @{
+$packageArgs = @{
   packageName    = 'Autodesk Desktop Connector'
   fileType       = 'exe'
-  file           = $file
+  url            = $url
   softwareName   = 'Autodesk Desktop Connector*'
+  checksum       = $checksum
+  checksumType   = 'sha256'
   silentArgs     = '-q'
   validExitCodes = @(0, 3010, 1641)
 }
-Install-ChocolateyInstallPackage @packageArgs
+Install-ChocolateyPackage @packageArgs
 
 #Close software running in elevated mode
 Stop-Process -Name "DesktopConnector*" -Force
--- desktopconnector/tools/helpers.ps1
+++ desktopconnector/tools/helpers.ps1
@@ -1,13 +1,13 @@
-function Invoke-UninstallOld {
+function Invoke-Uninstall15 {
 Stop-Process -Name "DesktopConnector*" -Force
 if($svc = get-service FileSystemMonitorService -ErrorAction SilentlyContinue) { Stop-Service $svc }
-$packageName = 'Autodesk Desktop Connector 16?*'
-$FolderName = 'C:\Program Files\Autodesk\Desktop Connector\'
+$packageName = 'Autodesk Desktop Connector*'
+$packageVersion = '15*'
 $validExitCodes = @(0, 3010, 1603, 1605, 1614, 1641)
 Get-ItemProperty -Path @('HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
                          'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*') `
                  -ErrorAction:SilentlyContinue `
-| Where-Object {$_.DisplayName -like $packageName} `
+| Where-Object {$_.DisplayName -like $packageName -and $_.DisplayVersion -like $packageVersion} `
 | ForEach-Object {
 	$silentArgs = "$($_.PSChildName) /qn /norestart"
 	if($($_.PSChildName) -like '{*') { Uninstall-ChocolateyPackage -PackageName "$($_.DisplayName)" -FileType "msi" -SilentArgs "$($silentArgs)" -File '' -ValidExitCodes $validExitCodes }
