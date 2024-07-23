90717677636fb24851e7e1a444c721b8575ec664
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2024-07-23 09:02:17 +0200

    clickshare-desktop.4.39.0.8

--- clickshare-desktop/tools/chocolateyinstall.ps1
+++ clickshare-desktop/tools/chocolateyinstall.ps1
@@ -1,14 +1,17 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-. $toolsDir\helpers.ps1
-Invoke-UninstallOldClickShare
-
-# Get download link for the latest version
+# Get download link
 $DownloadLink = Invoke-WebRequest "https://www.barco.com/bin/barco/tde/downloadUrl.json?fileNumber=R3306194&tdeType=3" | ConvertFrom-Json
 $url = $DownloadLink.downloadUrl
-$checksum = '6A608EB3D8EA0C5E03E547566B9466493A6A2683585246AC0248462C7FF3CBD5'
+$checksum = '877397182F5304DC3E6C3FCF3ED468BE55C9AD61E97ECEAE16F0D99FE2E2F14B'
 
+# Check Package Parameters
+$pp = Get-PackageParameters
+if ($pp.'ExtendedDesktop') { $silent = "/qn ACCEPT_EULA=YES EXTENDED_DESKTOP=YES AUTO_UPDATE=NO" }
+else { $silent = "/qn ACCEPT_EULA=YES AUTO_UPDATE=NO" }
+
+#Download and Extract
 $packageArgsURL = @{
   packageName   = $env:ChocolateyPackageName
   file          = $url
@@ -18,13 +21,18 @@ $packageArgsURL = @{
 }
 Install-ChocolateyZipPackage @packageArgsURL
 
+#Remove old version
+. $toolsDir\helpers.ps1
+Invoke-UninstallOldClickShare
+
+#Install
 $file = Join-Path $env:TEMP 'ClickShare_Installer.msi'
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'msi'
   file          = $file
   softwareName  = 'clickshare*'
-  silentArgs    = "/qn ACCEPT_EULA=YES"
+  silentArgs    = $silent
   validExitCodes= @(0, 3010, 1641)
 }
 Install-ChocolateyInstallPackage @packageArgs
