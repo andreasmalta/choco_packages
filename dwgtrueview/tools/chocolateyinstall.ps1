099d03c2c5f2c718f2ce83ab03176bc33cc1edb8
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2025-03-31 22:49:56 +0200

    dwgtrueview.2026.25.1.60

--- dwgtrueview/tools/chocolateyinstall.ps1
+++ dwgtrueview/tools/chocolateyinstall.ps1
@@ -4,30 +4,30 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 #1 LANGUAGE SELECTION
 $pp = Get-PackageParameters
 if ($pp.'French') {
-$url1 = 'https://efulfillment.autodesk.com/NetSWDLD/ODIS/prd/2025/PLC0000037/0F8A47A4-5472-3C4A-BCA7-2DD7B5087450/SFX/DWGTrueView_2025_French_64bit_db_001_002.exe'
-$checksum1 = '8C4EEBA965EF2BC84939DF80BE157D9D6DFBE4384B696B0BEFD99B2D35C349C5'
-$file = $env:TEMP + '\DWGTrueView_2025_French_64bit_db_001_002.exe'
-$url2 = 'https://efulfillment.autodesk.com/NetSWDLD/ODIS/prd/2025/PLC0000037/0F8A47A4-5472-3C4A-BCA7-2DD7B5087450/SFX/DWGTrueView_2025_French_64bit_db_002_002.7z'
-$checksum2 = '7138AE3201E2E7A7F29DF752661990FC1B0627404A9C1B9FE040D1353534846A'
-$zip = $env:TEMP + '\DWGTrueView_2025_French_64bit_db_002_002.7z'
+$url1 = 'https://efulfillment.autodesk.com/NetSWDLD/ODIS/prd/2026/PLC0000037/F0A4D097-C6CF-3865-A8AA-8B0C4434EF86/SFX/DWGTrueView_2026_French_64bit_db_001_002.exe'
+$checksum1 = 'EC5D57E4400B2CC3988BACED06BDE9A7DE44F601EFD4C59CC268EB28EC32FF6C'
+$file = $env:TEMP + '\DWGTrueView_2026_French_64bit_db_001_002.exe'
+$url2 = 'https://efulfillment.autodesk.com/NetSWDLD/ODIS/prd/2026/PLC0000037/F0A4D097-C6CF-3865-A8AA-8B0C4434EF86/SFX/DWGTrueView_2026_French_64bit_db_002_002.7z'
+$checksum2 = 'A2DD8093E985B535ECC6E2BD83D68BB9DC5335713BC8CC71B006153DB003CC0C'
+$zip = $env:TEMP + '\DWGTrueView_2026_French_64bit_db_002_002.7z'
 }
 
 elseif ($pp.'Japanese') {
-$url1 = 'https://efulfillment.autodesk.com/NetSWDLD/ODIS/prd/2025/PLC0000037/66C61B31-687C-3B6F-A4AE-777273EB4AD2/SFX/DWGTrueView_2025_Japanese_64bit_db_001_002.exe'
-$checksum1 = 'F4062187DD403A42703D679588B9F868BDB603D8F4DA1A160931E55F17CA96C9'
-$file = $env:TEMP + '\DWGTrueView_2025_Japanese_64bit_db_001_002.exe'
-$url2 = 'https://efulfillment.autodesk.com/NetSWDLD/ODIS/prd/2025/PLC0000037/66C61B31-687C-3B6F-A4AE-777273EB4AD2/SFX/DWGTrueView_2025_Japanese_64bit_db_002_002.7z'
-$checksum2 = 'DA41ACFB1FD0426B8875F5EEAA239E213726548E7458943EAB4BCB15C871F6F2'
-$zip = $env:TEMP + '\DWGTrueView_2025_Japanese_64bit_db_002_002.7z'
+$url1 = 'https://efulfillment.autodesk.com/NetSWDLD/ODIS/prd/2026/PLC0000037/43214E3A-22E1-36F4-81A4-456CE5E0FC4F/SFX/DWGTrueView_2026_Japanese_64bit_db_001_002.exe'
+$checksum1 = '207D584B01C6E25D5FCC1B1842BB433D824A1F09F06FB13934B51C2473790449'
+$file = $env:TEMP + '\DWGTrueView_2026_Japanese_64bit_db_001_002.exe'
+$url2 = 'https://efulfillment.autodesk.com/NetSWDLD/ODIS/prd/2026/PLC0000037/43214E3A-22E1-36F4-81A4-456CE5E0FC4F/SFX/DWGTrueView_2026_Japanese_64bit_db_002_002.7z'
+$checksum2 = '243B33AEE77FE36324439737EFBDEB0D6CBCA01250DACD03F58180927638B88E'
+$zip = $env:TEMP + '\DWGTrueView_2026_Japanese_64bit_db_002_002.7z'
 }
 
 else {
-$url1 = 'https://efulfillment.autodesk.com/NetSWDLD/ODIS/prd/2025/PLC0000037/A6C769F8-A5A7-3306-9711-0E6F4C045BA2/SFX/DWGTrueView_2025_English_64bit_db_001_002.exe'
-$checksum1 = '56B5B5988719AC14A8644EE1AA82D79576842C09D5F71BBBC450C3E28FFAA99B'
-$file = $env:TEMP + '\DWGTrueView_2025_English_64bit_db_001_002.exe'
-$url2 = 'https://efulfillment.autodesk.com/NetSWDLD/ODIS/prd/2025/PLC0000037/A6C769F8-A5A7-3306-9711-0E6F4C045BA2/SFX/DWGTrueView_2025_English_64bit_db_002_002.7z'
-$checksum2 = 'B487729B8B7A2FEA46085F6BC889312D0F53F32D243351FF8CB4893DCEE72066'
-$zip = $env:TEMP + '\DWGTrueView_2025_English_64bit_db_002_002.7z'
+$url1 = 'https://efulfillment.autodesk.com/NetSWDLD/ODIS/prd/2026/PLC0000037/9506362F-6781-39A3-B671-2BDAF86819F8/SFX/DWGTrueView_2026_English_64bit_db_001_002.exe'
+$checksum1 = 'CC1EAB29E3639E452823512458C1685047E057004C073E33399148527A8E3DD7'
+$file = $env:TEMP + '\DWGTrueView_2026_English_64bit_db_001_002.exe'
+$url2 = 'https://efulfillment.autodesk.com/NetSWDLD/ODIS/prd/2026/PLC0000037/9506362F-6781-39A3-B671-2BDAF86819F8/SFX/DWGTrueView_2026_English_64bit_db_002_002.7z'
+$checksum2 = 'C692818F8338FEA594BD1C2747762319B471B9126119A706304590E8C0918787'
+$zip = $env:TEMP + '\DWGTrueView_2026_English_64bit_db_002_002.7z'
 }
 
 #2 DOWNLOAD
@@ -48,3 +48,20 @@ $packageArgs  = @{
   validExitCodes = @(0, 3010, 1641)
 }
 Install-ChocolateyInstallPackage @packageArgs
+
+#5 WAIT UNTIL INSTALLED
+Write-Host "Waiting for Autodesk Installer to finish, trying again in 30 seconds..."
+Start-Sleep 30
+while ($true) {
+    # (re)try
+    $processCHK = Get-Process | Where {$_.ProcessName -Like 'Installer'}
+    if ($processCHK -eq $null)
+    {
+        Write-Host "Autodesk Installer ready!"
+		# exit the loop
+        break;
+    }
+    # wait
+    Write-Host "Waiting for Autodesk Installer to finish, trying again in 30 seconds..."
+    Start-Sleep 30
+}
