37436ebe19c2d4bb7b53f665efc96478c8c7797b
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2025-03-31 20:49:59 +0200

    inventor.2026.30.0.17500

--- inventor/tools/chocolateyinstall.ps1
+++ inventor/tools/chocolateyinstall.ps1
@@ -2,12 +2,12 @@
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 #1 DOWNLOAD
-$url1 = 'https://efulfillment.autodesk.com/NetSWDLD/ODIS/prd/2025/INVPROSA/E578CB4E-CF55-3E4D-9A8A-213245140B78/SFX/Inventor_Professional_2025_English_Win_64bit_db_001_002.exe'
-$checksum1 = 'AC42F4C5845A3248A124BC206228C56BD6C49258D0491A59700F28214E41E9B7'
-$file = $env:TEMP + '\Inventor_Professional_2025_English_Win_64bit_db_001_002.exe'
-$url2 = 'https://efulfillment.autodesk.com/NetSWDLD/ODIS/prd/2025/INVPROSA/E578CB4E-CF55-3E4D-9A8A-213245140B78/SFX/Inventor_Professional_2025_English_Win_64bit_db_002_002.7z'
-$checksum2 = '105BAA282929203763E9DD87F2FD0A9AC4A364FE9BB7967955874B5482740B4F'
-$zip = $env:TEMP + '\Inventor_Professional_2025_English_Win_64bit_db_002_002.7z'
+$url1 = 'https://efulfillment.autodesk.com/NetSWDLD/ODIS/prd/2026/INVPROSA/4262E5C6-0D93-3251-AD48-1E947463A640/SFX/Inventor_Professional_2026_English_Win_64bit_db_001_002.exe'
+$checksum1 = '980C4D9E3F02E054DA197B7B51B5AA433A7963A637DBBB6196364C9D88B0BD72'
+$file = $env:TEMP + '\Inventor_Professional_2026_English_Win_64bit_db_001_002.exe'
+$url2 = 'https://efulfillment.autodesk.com/NetSWDLD/ODIS/prd/2026/INVPROSA/4262E5C6-0D93-3251-AD48-1E947463A640/SFX/Inventor_Professional_2026_English_Win_64bit_db_002_002.7z'
+$checksum2 = '32B98C8787134745848CBB167A6ADF4F986C7B44E0C9576ADDC155F8786AED27'
+$zip = $env:TEMP + '\Inventor_Professional_2026_English_Win_64bit_db_002_002.7z'
 Get-ChocolateyWebFile -PackageName 'EXE package' -FileFullPath $file -Url $url1 -Checksum $checksum1 -ChecksumType 'sha256'
 Get-ChocolateyWebFile -PackageName 'ZIP package' -FileFullPath $zip -Url $url2 -Checksum $checksum2 -ChecksumType 'sha256'
 
@@ -25,3 +25,20 @@ $packageArgs  = @{
   validExitCodes = @(0, 3010, 1641)
 }
 Install-ChocolateyInstallPackage @packageArgs
+
+#4 WAIT UNTIL INSTALLED
+Write-Host "Waiting for Autodesk Installer to finish, trying again in 30 seconds..."
+Start-Sleep 30
+while ($true) {
+    # (re)try
+    $processCHK = Get-Process | Where {$_.ProcessName -Like 'Installer'}
+    if ($processCHK -eq $null)
+    {
+        Write-Host "Autodesk Installer ready!"
+		# exit the loop
+        break;
+    }
+    # wait
+    Write-Host "Waiting for Autodesk Installer to finish, trying again in 30 seconds..."
+    Start-Sleep 30
+}
