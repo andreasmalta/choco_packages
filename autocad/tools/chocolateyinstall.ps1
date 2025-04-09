1e21ff327899b460b6f41d0a98c2274747b02f4e
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2025-04-09 23:13:11 +0200

    autocad.2026.25.1.60

--- autocad/tools/chocolateyinstall.ps1
+++ autocad/tools/chocolateyinstall.ps1
@@ -1,25 +1,29 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-#1 DOWNLOAD
-$url1 = 'https://efulfillment.autodesk.com/NetSWDLD/ODIS/prd/2026/ACD/E55B46A2-B58D-3ABE-B345-8368D1AB33AA/SFX/SFX/ACD_2026_english_us_win_db_001_002.exe'
-$checksum1 = '4636C37DC7E94C6CE7702F853CB8EF9C85DACA700E89C8AB49C7D22F188E5040'
-$file = $env:TEMP + '\AutoCAD_2026_English_Win_64bit_db_001_002.exe'
-$url2 = 'https://efulfillment.autodesk.com/NetSWDLD/ODIS/prd/2026/ACD/E55B46A2-B58D-3ABE-B345-8368D1AB33AA/SFX/ACD_2026_english_us_win_db_002_002.7z'
-$checksum2 = '308D561E16A724335D47689EF065FD2C4224215FF168E7A4F21305888D7989BB'
-$zip = $env:TEMP + '\AutoCAD_2026_English_Win_64bit_db_002_002.7z'
-Get-ChocolateyWebFile -PackageName 'EXE package' -FileFullPath $file -Url $url1 -Checksum $checksum1 -ChecksumType 'sha256'
-Get-ChocolateyWebFile -PackageName 'ZIP package' -FileFullPath $zip -Url $url2 -Checksum $checksum2 -ChecksumType 'sha256'
+#1 DOWNLOAD & EXTRACT
+$url = 'https://efulfillment.autodesk.com/NetSWDLD/ODIS/prd/2026/ACD/E55B46A2-B58D-3ABE-B345-8368D1AB33AA/SFX/ACD_2026_english_us_win_db_002_002.7z'
+$checksum = '308D561E16A724335D47689EF065FD2C4224215FF168E7A4F21305888D7989BB'
+$tempversion = Join-Path $env:TEMP (Join-Path $env:ChocolateyPackageName $env:ChocolateyPackageVersion)
+$packageArgsUnzip = @{
+  packageName    = 'ACD Installation Files'
+  url            = $url
+  checksum       = $checksum
+  checksumType   = 'sha256'
+  unziplocation  = $tempversion
+}
+Install-ChocolateyZipPackage @packageArgsUnzip
 
 #2 UNINSTALL OLD
 . $toolsDir\helpers.ps1
 Invoke-UninstallAutoCAD
 
 #3 INSTALL
+$setup = Join-Path $tempversion 'setup.exe'
 $packageArgs  = @{
   packageName    = 'AutoCAD'
   fileType       = 'exe'
-  file           = $file
+  file           = $setup
   softwareName   = 'AutoCAD*'
   silentArgs     = '-q'
   validExitCodes = @(0, 3010, 1641)
