d800ff41e164093f0d1e3439d01a5dfe5f0857e3
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2025-04-10 23:42:20 +0200

    designreview.15.0.2.10

--- designreview/tools/chocolateyinstall.ps1
+++ designreview/tools/chocolateyinstall.ps1
@@ -1,28 +1,15 @@
 ﻿$ErrorActionPreference = 'Stop';
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url      = 'https://download.autodesk.com/us/support/files/designreview/2018/EXE/enu/SetupDesignReview.exe'
-$checksum_url    = '5258D6F5C26FE59A245A2540ADD6E49CC80025EEA48289F3ED30424EB83F3325'
-$extract = Join-Path $toolsDir 'setup'
-$file = Join-Path $toolsDir 'setup\x86\ADR\SetupDesignReview.msi'
-$checksum_file    = 'A54C237AAAC1570965AC61FAB480658A413955CD2151EAE02759D1F35A1CDAE1'
 
-$packageArgs1 = @{
+$url = "https://download.autodesk.com/us/support/files/designreview/2024/designreview-installer_en.exe"
+$checksum = '0D60D24A033820C80FD4BAE9406CF98DAA8D013E2331E794E297CE321AFEF2EC'
+$packageArgs = @{
   packageName   = $env:ChocolateyPackageName
+  fileType      = 'exe'
   url           = $url
-  checksum      = $checksum_url
+  softwareName  = 'Design Review*'
+  checksum      = $checksum
   checksumType  = 'sha256'
-  unziplocation = $extract
-}
-Install-ChocolateyZipPackage @packageArgs1
-
-$packageArgs2 = @{
-  packageName   = $env:ChocolateyPackageName
-  fileType      = 'msi'
-  file          = $file
-  softwareName  = 'designreview*'
-  checksum      = $checksum_file
-  checksumType  = 'sha256'
-  silentArgs    = "ALLUSERS=1 /q"
+  silentArgs    = "-q"
   validExitCodes= @(0, 3010, 1641)
 }
-Install-ChocolateyInstallPackage @packageArgs2
+Install-ChocolateyPackage @packageArgs
--- designreview/tools/chocolateyuninstall.ps1
+++ designreview/tools/chocolateyuninstall.ps1
@@ -1,7 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop';
+
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  fileType      = 'msi'
-  silentArgs    = "{139C013B-5BAC-4101-BC6C-B2A78C0125A4} /qn /norestart"
+  fileType      = 'exe'
+  silentArgs    = '-i uninstall --trigger_point system -m C:\ProgramData\Autodesk\ODIS\metadata\{96FBAF50-A8E7-38E4-82D5-B146E5EA8935}\bundleManifest.xml -x C:\ProgramData\Autodesk\ODIS\metadata\{96FBAF50-A8E7-38E4-82D5-B146E5EA8935}\SetupRes\manifest.xsd -q'
+  file			= 'C:\Program Files\Autodesk\AdODIS\V1\Installer.exe'
 }
 Uninstall-ChocolateyPackage @packageArgs
