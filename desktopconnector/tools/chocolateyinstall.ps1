1e80be7487e3ac181b3b3e93a3b609bd7731e052
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2025-05-23 21:30:37 +0200

    desktopconnector.16.14.1.2577

--- desktopconnector/tools/chocolateyinstall.ps1
+++ desktopconnector/tools/chocolateyinstall.ps1
@@ -1,13 +1,15 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
+#1 URL
 $url = "https://up.autodesk.com/2017/CDX/AB4AADCC-F890-4B4F-A7A6-B0FBD2386796/DesktopConnector-x64-" + $Env:ChocolateyPackageVersion + ".exe"
-#$url = "https://up.autodesk.com/2017/CDX/AB4AADCC-F890-4B4F-A7A6-B0FBD2386796/DesktopConnector-x64.exe"
-$checksum = 'F714E177D0A0A2BC3EA2F53C5962215532F23EEFFDA33BA11674B48C89EA5938'
+$checksum = '6623CC89FB81FEDD2401AE8D485A62AF058CC1DADB99E9252995D8A5ACBE91D0'
 
+#2 UNINSTALL 15
 . $toolsDir\helpers.ps1
 Invoke-Uninstall15
 
+#3 INSTALL
 $packageArgs = @{
   packageName    = 'Autodesk Desktop Connector'
   fileType       = 'exe'
@@ -20,5 +22,23 @@ $packageArgs = @{
 }
 Install-ChocolateyPackage @packageArgs
 
-#Close software running in elevated mode
+#4 WAIT UNTIL INSTALLED
+Write-Host "Waiting for Autodesk Installer to finish, trying again in 30 seconds..."
+Start-Sleep 30
+while ($true) {
+    # (re)try
+    $processCHK = Get-Process | Where {$_.ProcessName -Like 'Installer'}
+    if ($processCHK -eq $null)
+    {
+        Write-Host "Autodesk Installer ready!"
+		# exit the loop
+        break;
+    }
+    # wait
+    Write-Host "Waiting for Autodesk Installer to finish, trying again in 30 seconds..."
+    Start-Sleep 30
+}
+
+#5 Close software running in elevated mode
+Start-Sleep 30
 Stop-Process -Name "DesktopConnector*" -Force
