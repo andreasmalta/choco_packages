1e1fb0519e119b1cef149227747a8db042f3f1ae
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2025-07-12 22:03:14 +0200

    navfree.2026.23.0.143276

--- navfree/tools/chocolateyinstall.ps1
+++ navfree/tools/chocolateyinstall.ps1
@@ -2,15 +2,10 @@
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 #1 DOWNLOAD
-$url1 = 'https://dds.autodesk.com/NetSWDLD/ODIS/prd/2025/NAVFREE/D3B0AE93-EFDC-396F-8A81-AAA853DF7637/SFX/Autodesk_Navisworks_Freedom_2025_Win_64bit_db_001_002.exe'
-$checksum1 = '4B8DB2FA87F0B6DAE7589DAEEFD5F24D85B3057FC5796BFA79B0D453AF429336'
-$file = $env:TEMP + '\Autodesk_Navisworks_Freedom_2025_Win_64bit_db_001_002.exe'
-Get-ChocolateyWebFile -PackageName 'EXE package' -FileFullPath $file -Url $url1 -Checksum $checksum1 -ChecksumType 'sha256'
-
-$url2 = 'https://dds.autodesk.com/NetSWDLD/ODIS/prd/2025/NAVFREE/D3B0AE93-EFDC-396F-8A81-AAA853DF7637/SFX/Autodesk_Navisworks_Freedom_2025_Win_64bit_db_002_002.7z'
-$checksum2 = 'CB56B76A93448F02D12FDFAC1957F597F32C5911ECDABB6B886902FF1D60BC07'
-$zip = $env:TEMP + '\Autodesk_Navisworks_Freedom_2025_Win_64bit_db_002_002.7z'
-Get-ChocolateyWebFile -PackageName 'ZIP package' -FileFullPath $zip -Url $url2 -Checksum $checksum2 -ChecksumType 'sha256'
+$url = 'https://dds.autodesk.com/NetSWDLD/ODIS/prd/2026/NAVFREE/BE06C262-73A9-3C2F-8982-C105E1EE9A34/SFX/Autodesk_Navisworks_Freedom_2026_Win_64bit_db.sfx.exe'
+$checksum = '64E6987FB20244AC945378092075363E67FB7684161FFE1D20ED9E4420657A47'
+$file = $env:TEMP + '\Autodesk_Navisworks_Freedom_2026_Win_64bit_db.sfx.exe'
+Get-ChocolateyWebFile -PackageName 'EXE package' -FileFullPath $file -Url $url -Checksum $checksum -ChecksumType 'sha256'
 
 #2 UNINSTALL OLD VERSIONS
 . $toolsDir\helpers.ps1
@@ -18,7 +13,7 @@ Invoke-UninstallOld
 
 #3 INSTALL
 $packageArgs  = @{
-  packageName    = 'Navisworks Freedom'
+  packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
   file           = $file
   softwareName   = 'Navisworks Freedom*'
@@ -26,3 +21,20 @@ $packageArgs  = @{
   validExitCodes = @(0, 3010, 1641)
 }
 Install-ChocolateyInstallPackage @packageArgs
+
+#4 WAIT UNTIL INSTALLED
+Write-Host "Waiting for Autodesk Installer to finish..."
+Start-Sleep 120
+while ($true) {
+    # (re)try
+    $processCHK = Get-Process | Where {$_.ProcessName -Like 'Installer'}
+    if ($processCHK -eq $null)
+    {
+        Write-Host "Autodesk Installer ready!"
+		# exit the loop
+        break;
+    }
+    # wait
+    Write-Host "Waiting for Autodesk Installer to finish, trying again in 30 seconds..."
+    Start-Sleep 30
+}
