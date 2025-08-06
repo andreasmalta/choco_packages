4795232a03bab7299133e12bfeef487a320a438b
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2025-08-06 08:38:46 +0200

    desktopconnector.16.16.0.2792

--- desktopconnector/tools/chocolateyinstall.ps1
+++ desktopconnector/tools/chocolateyinstall.ps1
@@ -2,9 +2,9 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 #1 URL
-#$url = "https://up.autodesk.com/2017/CDX/AB4AADCC-F890-4B4F-A7A6-B0FBD2386796/DesktopConnector-x64-" + $Env:ChocolateyPackageVersion + ".exe"
-$url = "https://up.autodesk.com/2017/CDX/AB4AADCC-F890-4B4F-A7A6-B0FBD2386796/DesktopConnector-x64.exe"
-$checksum = '1BDD365C4D63ACE4638D83557BFE90CA46301E78CF98541947793080475EFAA4'
+$url = "https://up.autodesk.com/2017/CDX/AB4AADCC-F890-4B4F-A7A6-B0FBD2386796/DesktopConnector-x64-" + $Env:ChocolateyPackageVersion + ".exe"
+#$url = "https://up.autodesk.com/2017/CDX/AB4AADCC-F890-4B4F-A7A6-B0FBD2386796/DesktopConnector-x64.exe"
+$checksum = '9499E0ED756213DF2A28B679A1457E463D53053253F8AD005734F7AB9E40674D'
 
 #2 UNINSTALL 15
 . $toolsDir\helpers.ps1
