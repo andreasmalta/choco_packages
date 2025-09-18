82d73d658ab29f58a7bd4581fc24cd5ed5915405
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2025-09-18 19:14:52 +0200

    brightauthorconnected.1.67.0

--- brightauthorconnected/tools/chocolateyinstall.ps1
+++ brightauthorconnected/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop';
 
-$url = "https://downloads.bsn.cloud/BA%20connected%20Setup%201.63.2.exe"
-$checksum = 'AD0E22BB364E59BCC8E8A20D8CBBC4464ADA24A599B57B62B2CACAFAF08D74C4'
+$url = 'https://downloads.bsn.cloud/BA%20connected%20Setup%201.67.0.exe'
+$checksum = '2F38ACF4B56F1C0B0EE4CF634768D09D44391348D7ACA64B461156EA6141B7AA'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -10,7 +10,7 @@ $packageArgs = @{
   softwareName  = 'BAconnected*'
   checksum      = $checksum
   checksumType  = 'sha256'
-  silentArgs    = "/S"
+  silentArgs    = '/S'
   validExitCodes= @(0, 3010, 1641)
 }
 
