d236eb146de7c3703ce9283c42882513946093ac
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2026-04-06 22:36:08 +0200

    inventor.2027.31.0.19200

--- inventor/tools/chocolateyinstall.ps1
+++ inventor/tools/chocolateyinstall.ps1
@@ -1,25 +1,29 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-#1 DOWNLOAD
-$url1 = 'https://efulfillment.autodesk.com/NetSWDLD/ODIS/prd/2026/INVPROSA/4262E5C6-0D93-3251-AD48-1E947463A640/SFX/Inventor_Professional_2026_English_Win_64bit_db_001_002.exe'
-$checksum1 = '980C4D9E3F02E054DA197B7B51B5AA433A7963A637DBBB6196364C9D88B0BD72'
-$file = $env:TEMP + '\Inventor_Professional_2026_English_Win_64bit_db_001_002.exe'
-$url2 = 'https://efulfillment.autodesk.com/NetSWDLD/ODIS/prd/2026/INVPROSA/4262E5C6-0D93-3251-AD48-1E947463A640/SFX/Inventor_Professional_2026_English_Win_64bit_db_002_002.7z'
-$checksum2 = '32B98C8787134745848CBB167A6ADF4F986C7B44E0C9576ADDC155F8786AED27'
-$zip = $env:TEMP + '\Inventor_Professional_2026_English_Win_64bit_db_002_002.7z'
-Get-ChocolateyWebFile -PackageName 'EXE package' -FileFullPath $file -Url $url1 -Checksum $checksum1 -ChecksumType 'sha256'
-Get-ChocolateyWebFile -PackageName 'ZIP package' -FileFullPath $zip -Url $url2 -Checksum $checksum2 -ChecksumType 'sha256'
+#1 DOWNLOAD & EXTRACT
+$url = 'https://efulfillment.autodesk.com/NetSWDLD/ODIS/prd/2027/INVPROSA/447AF3F0-5A11-3D54-B8C1-9D1C88C26148/SFX/Inventor_Professional_2027_English_Win_64bit_db_002_002.7z'
+$checksum = '87A0196BD51EEB046695F7D2EEE477DB162378C5D03060403709F53D05DB33FD'
+$tempversion = Join-Path $env:TEMP (Join-Path $env:ChocolateyPackageName $env:ChocolateyPackageVersion)
+$packageArgsUnzip = @{
+  packageName    = 'Inventor Installation Files'
+  url            = $url
+  checksum       = $checksum
+  checksumType   = 'sha256'
+  unziplocation  = $tempversion
+}
+Install-ChocolateyZipPackage @packageArgsUnzip
 
 #2 UNINSTALL OLD
 . $toolsDir\helpers.ps1
 Invoke-UninstallInventor
 
 #3 INSTALL
+$setup = Join-Path $tempversion 'setup.exe'
 $packageArgs  = @{
   packageName    = 'Inventor'
   fileType       = 'exe'
-  file           = $file
+  file           = $setup
   softwareName   = 'Inventor*'
   silentArgs     = '-q'
   validExitCodes = @(0, 3010, 1641)
@@ -27,8 +31,8 @@ $packageArgs  = @{
 Install-ChocolateyInstallPackage @packageArgs
 
 #4 WAIT UNTIL INSTALLED
-Write-Host "Waiting for Autodesk Installer to finish, trying again in 30 seconds..."
-Start-Sleep 30
+Write-Host "Waiting for Autodesk Installer to finish..."
+Start-Sleep 120
 while ($true) {
     # (re)try
     $processCHK = Get-Process | Where {$_.ProcessName -Like 'Installer'}
