1904108f886f99298d27962f5f3cafca6ba17850
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2026-04-12 20:59:36 +0200

    3dsmax.2027.29.0.3590

---
+++ 3dsmax/tools/chocolateyinstall.ps1
@@ -0,0 +1,48 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+#1 DOWNLOAD & EXTRACT
+$url = 'https://efulfillment.autodesk.com/NetSWDLD/ODIS/prd/2027/3DSMAX/F157F7D2-C9E6-3D58-BADD-20AF6B16F6BF/SFX/Autodesk_3ds_Max_2027_EFGJKPS_Win_64bit_db_002_002.7z'
+$checksum = 'CD06416CD2928433ACFFF32B8A56FE3C9DF13800B1DCC7D69F03C4BC3071A9FE'
+$tempversion = Join-Path $env:TEMP (Join-Path $env:ChocolateyPackageName $env:ChocolateyPackageVersion)
+$packageArgsUnzip = @{
+  packageName    = 'Installation Files'
+  url            = $url
+  checksum       = $checksum
+  checksumType   = 'sha256'
+  unziplocation  = $tempversion
+}
+Install-ChocolateyZipPackage @packageArgsUnzip
+
+#2 UNINSTALL OLD
+. $toolsDir\helpers.ps1
+Invoke-UninstallOld
+
+#3 INSTALL
+$setup = Join-Path $tempversion 'setup.exe'
+$packageArgs  = @{
+  packageName    = '3ds Max'
+  fileType       = 'exe'
+  file           = $setup
+  softwareName   = '3ds Max*'
+  silentArgs     = '-q'
+  validExitCodes = @(0, 3010, 1641)
+}
+Install-ChocolateyInstallPackage @packageArgs
+
+#4 WAIT UNTIL INSTALLED
+Write-Host "Waiting for Autodesk Installer to finish..."
+Start-Sleep 120
+while ($true) {
+    # (re)try
+    $processCHK = Get-Process | Where {$_.ProcessName -Like 'Installer'}
+    if ($processCHK -eq $null)
+    {
+        Write-Host "Autodesk Installer ready!"
+		# exit the loop
+        break;
+    }
+    # wait
+    Write-Host "Waiting for Autodesk Installer to finish, trying again in 30 seconds..."
+    Start-Sleep 30
+}
