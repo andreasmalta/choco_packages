a773b11634e46f2719a20f6c167f824fa9b28070
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2026-04-13 20:46:54 +0200

    civ3d.2027.13.9.628

---
+++ civ3d/tools/chocolateyinstall.ps1
@@ -0,0 +1,48 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+#1 DOWNLOAD & EXTRACT
+$url = 'https://efulfillment.autodesk.com/NetSWDLD/ODIS/prd/2027/CIV3D/01D11119-5E52-3ED3-8B05-87AAED2B0754/SFX/CIV3D_2027_english_us_win_db_002_002.7z'
+$checksum = 'E333E4AE9AA829A86F037C522431665A68E6EB25468D7A90EA075BEFD746BECD'
+$tempversion = Join-Path $env:TEMP (Join-Path $env:ChocolateyPackageName $env:ChocolateyPackageVersion)
+$packageArgsUnzip = @{
+  packageName    = 'Installation Files'
+  url            = $url
+  checksum       = $checksum
+  checksumType   = 'sha256'
+  unziplocation  = $tempversion
+}
+Install-ChocolateyZipPackage @packageArgsUnzip
+
+#2 UNINSTALL OLD
+. $toolsDir\helpers.ps1
+Invoke-UninstallOld
+
+#3 INSTALL
+$setup = Join-Path $tempversion 'setup.exe'
+$packageArgs  = @{
+  packageName    = 'Civil 3D'
+  fileType       = 'exe'
+  file           = $setup
+  softwareName   = 'Civil 3D*'
+  silentArgs     = '-q'
+  validExitCodes = @(0, 3010, 1641)
+}
+Install-ChocolateyInstallPackage @packageArgs
+
+#4 WAIT UNTIL INSTALLED
+Write-Host "Waiting for Autodesk Installer to finish..."
+Start-Sleep 120
+while ($true) {
+    # (re)try
+    $processCHK = Get-Process | Where {$_.ProcessName -Like 'Installer'}
+    if ($processCHK -eq $null)
+    {
+        Write-Host "Autodesk Installer ready!"
+		# exit the loop
+        break;
+    }
+    # wait
+    Write-Host "Waiting for Autodesk Installer to finish, trying again in 30 seconds..."
+    Start-Sleep 30
+}
---
+++ civ3d/tools/chocolateyuninstall.ps1
@@ -0,0 +1,6 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+#UNINSTALL MAIN SOFTWARE
+. $toolsDir\helpers.ps1
+Invoke-UninstallOld
