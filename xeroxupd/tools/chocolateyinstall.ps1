841da02b4a51210e760f1bcbfbb3f776f2d53f5d
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2026-05-30 08:54:51 +0200

    xeroxupd.5.1076.4

--- xeroxupd/tools/chocolateyinstall.ps1
+++ xeroxupd/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$PCL64 = 'https://download.support.xerox.com/pub/drivers/GLOBALPRINTDRIVER/drivers/win10x64/ar/UNIV_5.1076.3.0_PCL6_x64.zip'
-$PCL64_checksum = '8516A55AAD5C5406CF5DA5DE998897FCB7CFD5D99724C221AFA519F5317F7F68'
-$PCL86 = 'https://download.support.xerox.com/pub/drivers/GLOBALPRINTDRIVER/drivers/win10/ar/UNIV_5.1076.3.0_PCL6_x86.zip'
-$PCL86_checksum = '1B9D94F5E69AC93E75D2E1C384389958E18317334BBA39CE78B8B19819B1D04B'
-$PS64 = 'https://download.support.xerox.com/pub/drivers/VLC8000W/drivers/win10x64/ar/UNIV_5.1076.3.0_PS_x64.zip'
-$PS64_checksum = '90D79E7E1E6908469C9340180161FA2C072FAAB7E86788A774541D08B855E2C3'
-$PS86 = 'https://download.support.xerox.com/pub/drivers/VLC8000W/drivers/win10/ar/UNIV_5.1076.3.0_PS_x86.zip'
-$PS86_checksum = '646D6428385E906A120D9A772AD251E1A6C9A959FB11E583CE5BB7DBA9A11C2F'
+$PCL64 = 'https://download.support.xerox.com/pub/drivers/GLOBALPRINTDRIVER/drivers/win10x64/ar/UNIV_5.1076.4.0_PCL6_x64.zip'
+$PCL64_checksum = '09FCAE9C84572D30656F4CD56AE8B59CEA0D1653B83B8F96256754DC9EABBEAB'
+$PCL86 = 'https://download.support.xerox.com/pub/drivers/GLOBALPRINTDRIVER/drivers/win10/ar/UNIV_5.1076.4.0_PCL6_x86.zip'
+$PCL86_checksum = '27D8543F9AF8F12259F329B38495E41520AA1D74C9846C16201BA454DBB31307'
+$PS64 = 'https://download.support.xerox.com/pub/drivers/VLC8000W/drivers/win10x64/ar/UNIV_5.1076.4.0_PS_x64.zip'
+$PS64_checksum = '444A894CED690EEFE708770D6700F276EBC759312929517A11477D620108D4A4'
+$PS86 = 'https://download.support.xerox.com/pub/drivers/VLC8000W/drivers/win10/ar/UNIV_5.1076.4.0_PS_x86.zip'
+$PS86_checksum = '83BC0EEB04A75FDB9950647CD751548F4EAAA510EA62FE7825868A53D6377BAC'
 $extract = Join-Path $env:TEMP 'extract'
 
 $packageArgsPCL = @{
