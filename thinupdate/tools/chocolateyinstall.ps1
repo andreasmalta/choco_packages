5b6e041f38a6c2f13cc410c153580705cf866ded
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2026-08-02 13:19:07 +0200

    thinupdate.2.7.52

--- thinupdate/tools/chocolateyinstall.ps1
+++ thinupdate/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$url = 'https://ftp.hp.com/pub/tcimages/EasyUpdate/Images/ThinUpdateRelease/2_7_49/HPThinUpdate-2.7.49-Win64.msi'
-$checksum = 'F715F749FDBDABA1817D29F28A14506C1B85573C8CC3968AB53750524C0F5E10'
+$url = 'https://ftp.hp.com/pub/tcimages/EasyUpdate/Images/ThinUpdateRelease/2_7_52/HPThinUpdate-2.7.52-Win64.msi'
+$checksum = 'EF656F4A844D2680E9C191E9267040425421D6265EEB55D047868F61A3CD22C6'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
