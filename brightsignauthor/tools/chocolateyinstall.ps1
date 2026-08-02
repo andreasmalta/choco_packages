174f1002ee212bb3974af2997936af7fd0a4440c
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2026-08-02 15:33:58 +0200

    brightsignauthor.1.85.0

---
+++ brightsignauthor/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+$ErrorActionPreference = 'Stop';
+
+$url = 'https://downloads.bsn.cloud/BA%20connected%20Setup%201.85.0.exe'
+$checksum = 'FDE739ECE5100A0C3D99249AD3862E5A0669F1C5EA5F72DE8E4ED2B60385FBAE'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'exe'
+  url           = $url
+  softwareName  = 'BA Connected*'
+  checksum      = $checksum
+  checksumType  = 'sha256'
+  silentArgs    = '/S'
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
