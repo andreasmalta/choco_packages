4006d497e171cf1900f7952f430ac8ba798fa49a
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2026-08-02 14:35:10 +0200

    desktopconnector.2027.2.0.85

--- desktopconnector/tools/chocolateyinstall.ps1
+++ desktopconnector/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 #1 URL
 #$url = "https://up.autodesk.com/2017/CDX/AB4AADCC-F890-4B4F-A7A6-B0FBD2386796/DesktopConnector-x64.exe"
 $url = "https://up.autodesk.com/2017/CDX/AB4AADCC-F890-4B4F-A7A6-B0FBD2386796/DesktopConnector-x64-" + $Env:ChocolateyPackageVersion + ".exe"
-$checksum = '99D4383486F697C6AE5D87CB4532DC0A208C2E83BF4361DC85748721DCA5A825'
+$checksum = 'F9759A8F0B81EFF86447CA33933A66AF5CE8A9128E83EAED05898CF2F8E7A715'
 
 . $toolsDir\helpers.ps1
 Invoke-Uninstall17
